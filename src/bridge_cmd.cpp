16c7d4f8b71db84460235d7378018716106bf1cd
cirdan <cirdansw@gmail.com>
2011-07-20 15:43:44 +0200

    Move bridge utility functions to bridge_cmd.cpp
    Move several bridge utility functions from tunnelbridge_cmd.cpp
    to bridge_cmd.cpp.

--- src/bridge_cmd.cpp
+++ src/bridge_cmd.cpp
@@ -11,3 +11,111 @@
  * @file bridge_cmd.cpp
  * This file deals with bridges (non-gui stuff)
  */
+
+#include "stdafx.h"
+#include "bridge.h"
+#include "bridge_map.h"
+#include "landscape.h"
+#include "slope_func.h"
+#include "command_func.h"
+#include "date_func.h"
+
+#include "table/sprites.h"
+#include "table/strings.h"
+#include "table/bridge_land.h"
+
+
+BridgeSpec _bridge[MAX_BRIDGES]; ///< The specification of all bridges.
+
+/** Reset the data been eventually changed by the grf loaded. */
+void ResetBridges()
+{
+	/* First, free sprite table data */
+	for (BridgeType i = 0; i < MAX_BRIDGES; i++) {
+		if (_bridge[i].sprite_table != NULL) {
+			for (BridgePieces j = BRIDGE_PIECE_NORTH; j < BRIDGE_PIECE_INVALID; j++) free(_bridge[i].sprite_table[j]);
+			free(_bridge[i].sprite_table);
+		}
+	}
+
+	/* Then, wipe out current bridges */
+	memset(&_bridge, 0, sizeof(_bridge));
+	/* And finally, reinstall default data */
+	memcpy(&_bridge, &_orig_bridge, sizeof(_orig_bridge));
+}
+
+/**
+ * Calculate the price factor for building a long bridge.
+ * Basically the cost delta is 1,1, 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5, 6,6,6,6,6,6,  7,7,7,7,7,7,7,  8,8,8,8,8,8,8,8,
+ * @param length Length of the bridge.
+ * @return Price factor for the bridge.
+ */
+int CalcBridgeLenCostFactor(int length)
+{
+	if (length <= 2) return length;
+
+	length -= 2;
+	int sum = 2;
+
+	int delta;
+	for (delta = 1; delta < length; delta++) {
+		sum += delta * delta;
+		length -= delta;
+	}
+	sum += delta * length;
+	return sum;
+}
+
+/**
+ * Get the foundation for a bridge.
+ * @param tileh The slope to build the bridge on.
+ * @param axis The axis of the bridge entrance.
+ * @return The foundation required.
+ */
+Foundation GetBridgeFoundation(Slope tileh, Axis axis)
+{
+	if (tileh == SLOPE_FLAT ||
+			((tileh == SLOPE_NE || tileh == SLOPE_SW) && axis == AXIS_X) ||
+			((tileh == SLOPE_NW || tileh == SLOPE_SE) && axis == AXIS_Y)) return FOUNDATION_NONE;
+
+	return (HasSlopeHighestCorner(tileh) ? InclinedFoundation(axis) : FlatteningFoundation(tileh));
+}
+
+/**
+ * Determines if the track on a bridge ramp is flat or goes up/down.
+ *
+ * @param tileh Slope of the tile under the bridge head
+ * @param axis Orientation of bridge
+ * @return true iff the track is flat.
+ */
+bool HasBridgeFlatRamp(Slope tileh, Axis axis)
+{
+	ApplyFoundationToSlope(GetBridgeFoundation(tileh, axis), &tileh);
+	/* If the foundation slope is flat the bridge has a non-flat ramp and vice versa. */
+	return (tileh != SLOPE_FLAT);
+}
+
+/**
+ * Is a bridge of the specified type and length available?
+ * @param bridge_type Wanted type of bridge.
+ * @param bridge_len  Wanted length of the bridge.
+ * @return A succeeded (the requested bridge is available) or failed (it cannot be built) command.
+ */
+CommandCost CheckBridgeAvailability(BridgeType bridge_type, uint bridge_len, DoCommandFlag flags)
+{
+	if (flags & DC_QUERY_COST) {
+		if (bridge_len <= _settings_game.construction.max_bridge_length) return CommandCost();
+		return_cmd_error(STR_ERROR_BRIDGE_TOO_LONG);
+	}
+
+	if (bridge_type >= MAX_BRIDGES) return CMD_ERROR;
+
+	const BridgeSpec *b = GetBridgeSpec(bridge_type);
+	if (b->avail_year > _cur_year) return CMD_ERROR;
+
+	uint max = min(b->max_length, _settings_game.construction.max_bridge_length);
+
+	if (b->min_length > bridge_len) return CMD_ERROR;
+	if (bridge_len <= max) return CommandCost();
+	return_cmd_error(STR_ERROR_BRIDGE_TOO_LONG);
+}
--- src/tunnelbridge_cmd.cpp
+++ src/tunnelbridge_cmd.cpp
@@ -44,78 +44,10 @@
 #include "table/strings.h"
 #include "table/bridge_land.h"
 
-BridgeSpec _bridge[MAX_BRIDGES]; ///< The specification of all bridges.
 TileIndex _build_tunnel_endtile; ///< The end of a tunnel; as hidden return from the tunnel build command for GUI purposes.
 
 /** Z position of the bridge sprites relative to bridge height (downwards) */
 static const int BRIDGE_Z_START = 3;
-
-/** Reset the data been eventually changed by the grf loaded. */
-void ResetBridges()
-{
-	/* First, free sprite table data */
-	for (BridgeType i = 0; i < MAX_BRIDGES; i++) {
-		if (_bridge[i].sprite_table != NULL) {
-			for (BridgePieces j = BRIDGE_PIECE_NORTH; j < BRIDGE_PIECE_INVALID; j++) free(_bridge[i].sprite_table[j]);
-			free(_bridge[i].sprite_table);
-		}
-	}
-
-	/* Then, wipe out current bridges */
-	memset(&_bridge, 0, sizeof(_bridge));
-	/* And finally, reinstall default data */
-	memcpy(&_bridge, &_orig_bridge, sizeof(_orig_bridge));
-}
-
-/**
- * Calculate the price factor for building a long bridge.
- * Basically the cost delta is 1,1, 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5, 6,6,6,6,6,6,  7,7,7,7,7,7,7,  8,8,8,8,8,8,8,8,
- * @param length Length of the bridge.
- * @return Price factor for the bridge.
- */
-int CalcBridgeLenCostFactor(int length)
-{
-	if (length < 2) return length;
-
-	length -= 2;
-	int sum = 2;
-	for (int delta = 1;; delta++) {
-		for (int count = 0; count < delta; count++) {
-			if (length == 0) return sum;
-			sum += delta;
-			length--;
-		}
-	}
-}
-
-/**
- * Get the foundation for a bridge.
- * @param tileh The slope to build the bridge on.
- * @param axis The axis of the bridge entrance.
- * @return The foundation required.
- */
-Foundation GetBridgeFoundation(Slope tileh, Axis axis)
-{
-	if (tileh == SLOPE_FLAT ||
-			((tileh == SLOPE_NE || tileh == SLOPE_SW) && axis == AXIS_X) ||
-			((tileh == SLOPE_NW || tileh == SLOPE_SE) && axis == AXIS_Y)) return FOUNDATION_NONE;
-
-	return (HasSlopeHighestCorner(tileh) ? InclinedFoundation(axis) : FlatteningFoundation(tileh));
-}
-
-/**
- * Determines if the track on a bridge ramp is flat or goes up/down.
- *
- * @param tileh Slope of the tile under the bridge head
- * @param axis Orientation of bridge
- * @return true iff the track is flat.
- */
-bool HasBridgeFlatRamp(Slope tileh, Axis axis)
-{
-	ApplyFoundationToSlope(GetBridgeFoundation(tileh, axis), &tileh);
-	/* If the foundation slope is flat the bridge has a non-flat ramp and vice versa. */
-	return (tileh != SLOPE_FLAT);
-}
 
 static inline const PalSpriteID *GetBridgeSpriteTable(int index, BridgePieces table)
 {
@@ -169,31 +101,6 @@ static CommandCost CheckBridgeSlopeSouth(Axis axis, Slope *tileh, int *z)
 	if (f == FOUNDATION_NONE) return CommandCost();
 
 	return CommandCost(EXPENSES_CONSTRUCTION, _price[PR_BUILD_FOUNDATION]);
-}
-
-/**
- * Is a bridge of the specified type and length available?
- * @param bridge_type Wanted type of bridge.
- * @param bridge_len  Wanted length of the bridge.
- * @return A succeeded (the requested bridge is available) or failed (it cannot be built) command.
- */
-CommandCost CheckBridgeAvailability(BridgeType bridge_type, uint bridge_len, DoCommandFlag flags)
-{
-	if (flags & DC_QUERY_COST) {
-		if (bridge_len <= _settings_game.construction.max_bridge_length) return CommandCost();
-		return_cmd_error(STR_ERROR_BRIDGE_TOO_LONG);
-	}
-
-	if (bridge_type >= MAX_BRIDGES) return CMD_ERROR;
-
-	const BridgeSpec *b = GetBridgeSpec(bridge_type);
-	if (b->avail_year > _cur_year) return CMD_ERROR;
-
-	uint max = min(b->max_length, _settings_game.construction.max_bridge_length);
-
-	if (b->min_length > bridge_len) return CMD_ERROR;
-	if (bridge_len <= max) return CommandCost();
-	return_cmd_error(STR_ERROR_BRIDGE_TOO_LONG);
 }
 
 /**
