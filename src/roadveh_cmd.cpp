6bc17ffa2d1140068c189eabef2409c91be17c9d
cirdan <cirdansw@gmail.com>
2011-09-26 23:31:21 +0200

    Use DistanceFromTileEdge in road tunnel enter proc
    Simplify handling of road tunnels by using DistanceFromTileEdge to
    detect when a vehicle must be hidden or becomes visible again.

--- src/roadveh_cmd.cpp
+++ src/roadveh_cmd.cpp
@@ -745,31 +745,22 @@ static VehicleEnterTileStatus RoadVehEnter_Misc(RoadVehicle *u, TileIndex tile, 
 		default: break;
 
 		case TT_MISC_TUNNEL: {
-			int z = GetSlopePixelZ(x, y) - u->z_pos;
-			assert(abs(z) < 3);
+			assert(abs((int)GetSlopePixelZ(x, y) - u->z_pos) < 3);
 
 			/* Direction into the wormhole */
 			const DiagDirection dir = GetTunnelBridgeDirection(tile);
-			/* Direction of the vehicle */
-			const DiagDirection vdir = DirToDiagDir(u->direction);
-			/* New position of the vehicle on the tile */
-			byte pos = (DiagDirToAxis(vdir) == AXIS_X ? x : y) & TILE_UNIT_MASK;
-			/* Number of units moved by the vehicle since entering the tile */
-			byte frame = (vdir == DIAGDIR_NE || vdir == DIAGDIR_NW) ? TILE_SIZE - 1 - pos : pos;
 
-			/* Enter tunnel? */
-			if (u->state != RVSB_WORMHOLE && dir == vdir) {
+			if (u->direction == DiagDirToDir(dir)) {
+				uint frame = DistanceFromTileEdge(ReverseDiagDir(dir), x & 0xF, y & 0xF);
 				if (frame == _tunnel_visibility_frame[dir]) {
 					/* Frame should be equal to the next frame number in the RV's movement */
-					assert(frame == u->frame + 1);
+					assert((int)frame == u->frame + 1);
 					u->vehstatus |= VS_HIDDEN;
 				}
-				return VETS_CONTINUE;
-			}
-
-			/* We're at the tunnel exit ?? */
-			if (dir == ReverseDiagDir(vdir) && z == 0) {
+			} else if (u->direction == ReverseDir(DiagDirToDir(dir))) {
+				uint frame = DistanceFromTileEdge(dir, x & 0xF, y & 0xF);
 				if (frame == TILE_SIZE - _tunnel_visibility_frame[dir]) {
+					assert((int)frame == u->frame + 1);
 					u->vehstatus &= ~VS_HIDDEN;
 				}
 			}
