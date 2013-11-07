1e0c6270deb721354f50a72d588ac8e12abc739e
frosch <frosch@openttd.org>
2013-11-07 18:17:21 +0000

    Add vehicle chain inspection in newgrf debug window
    (openttd r25946) -Add: [NewGRF Debuggnig] Inspecting other vehicles
    in a chain.

--- src/train_cmd.cpp
+++ src/train_cmd.cpp
@@ -36,6 +36,7 @@
 #include "zoom_func.h"
 #include "signal_func.h"
 #include "station_func.h"
+#include "newgrf_debug.h"
 
 #include "table/strings.h"
 #include "table/train_cmd.h"
@@ -223,6 +224,7 @@ void Train::ConsistChanged(bool same_length)
 		SetWindowDirty(WC_VEHICLE_DETAILS, this->index);
 		InvalidateWindowData(WC_VEHICLE_REFIT, this->index, VIWD_CONSIST_CHANGED);
 		InvalidateWindowData(WC_VEHICLE_ORDERS, this->index, VIWD_CONSIST_CHANGED);
+		InvalidateNewGRFInspectWindow(GSF_TRAINS, this->index);
 	}
 }
 
@@ -1367,6 +1369,7 @@ CommandCost CmdMoveRailVehicle(TileIndex tile, DoCommandFlag flags, uint32 p1, u
 			DeleteWindowById(WC_VEHICLE_REFIT, src->index);
 			DeleteWindowById(WC_VEHICLE_DETAILS, src->index);
 			DeleteWindowById(WC_VEHICLE_TIMETABLE, src->index);
+			DeleteNewGRFInspectWindow(GSF_TRAINS, src->index);
 			SetWindowDirty(WC_COMPANY, _current_company);
 
 			/* Delete orders, group stuff and the unit number as we're not the
