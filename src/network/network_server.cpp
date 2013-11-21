2d3abac77f3aa97bf976a2c1b6b56189a4617f25
rubidium <rubidium@openttd.org>
2013-11-21 18:35:31 +0000

    Move handling of async save finish
    (openttd r26043) -Fix [FS#5811]: occasional hanging when client joins

--- src/network/network_server.cpp
+++ src/network/network_server.cpp
@@ -114,6 +114,12 @@ struct PacketWriter : SaveFilter {
 		if (this->mutex != NULL) this->mutex->SendSignal();
 
 		if (this->mutex != NULL) this->mutex->EndCritical();
+
+		/* Make sure the saving is completely cancelled. Yes,
+		 * we need to handle the save finish as well as the
+		 * next connection might just be requesting a map. */
+		WaitTillSaved();
+		ProcessAsyncSaveFinish();
 	}
 
 	/**
@@ -237,13 +243,6 @@ ServerNetworkGameSocketHandler::~ServerNetworkGameSocketHandler()
 		this->savegame->Destroy();
 		this->savegame = NULL;
 	}
-
-	/* Make sure the saving is completely cancelled.
-	 * Yes, we need to handle the save finish as well
-	 * as the next connection in this "loop" might
-	 * just be requesting the map and such. */
-	WaitTillSaved();
-	ProcessAsyncSaveFinish();
 }
 
 Packet *ServerNetworkGameSocketHandler::ReceivePacket()
@@ -622,8 +621,6 @@ NetworkRecvStatus ServerNetworkGameSocketHandler::SendMap()
 			/* Done reading, make sure saving is done as well */
 			this->savegame->Destroy();
 			this->savegame = NULL;
-
-			WaitTillSaved();
 
 			/* Set the status to DONE_MAP, no we will wait for the client
 			 *  to send it is ready (maybe that happens like never ;)) */
