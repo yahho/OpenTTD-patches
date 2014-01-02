d6214e5313a2a396e5f57fc36371fb94ab156b31
rubidium <rubidium@openttd.org>
2014-01-02 22:41:58 +0000

    Rework blitter factories to reduce template usage
    (openttd r26209) -Codechange: remove some template magic and
    simplify some code

--- src/smallmap_gui.cpp
+++ src/smallmap_gui.cpp
@@ -960,7 +960,7 @@ void SmallMapWindow::DrawMapIndicators() const
  */
 void SmallMapWindow::DrawSmallMap(DrawPixelInfo *dpi) const
 {
-	Blitter *blitter = BlitterFactoryBase::GetCurrentBlitter();
+	Blitter *blitter = BlitterFactory::GetCurrentBlitter();
 	DrawPixelInfo *old_dpi;
 
 	old_dpi = _cur_dpi;
