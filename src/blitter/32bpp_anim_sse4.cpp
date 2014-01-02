c31f2a670a6b5b3959beacb0bc373ba1f28d6df5
rubidium <rubidium@openttd.org>
2014-01-02 23:52:13 +0000

    Add animated SSE4 blitter
    (openttd r26214) -Add: specialised animated SSE4 blitter (MJP)
    With 32bpp base set about 15-20% faster in the Draw function
    (slower with 8bpp base set). Overall, with 32bpp base set,
    about 5% faster.
    
    (openttd r26218) -Fix: wrong #if for SSE in source.list
    
    (openttd r26221) -Codechange: normalize constant names in
    anim-sse4 with the others (MJP)
    
    (openttd r26247) -Fix [FS#5854, FS#5855]: Possible out of bounds
    reads with the sse blitters (MJP)

--- src/blitter/32bpp_anim_sse4.cpp
+++ src/blitter/32bpp_anim_sse4.cpp
@@ -7,17 +7,17 @@
  * See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with OpenTTD. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/** @file 32bpp_sse4.cpp Implementation of the SSE4 32 bpp blitter. */
+/** @file 32bpp_sse4_anim.cpp Implementation of the SSE4 32 bpp blitter with animation support. */
 
 #ifdef WITH_SSE
 
 #include "../stdafx.h"
-#include "../zoom_func.h"
-#include "../settings_type.h"
-#include "32bpp_sse4.hpp"
+#include "../video/video_driver.hpp"
+#include "../table/sprites.h"
+#include "32bpp_anim_sse4.hpp"
 
 /** Instantiation of the SSE4 32bpp blitter factory. */
-static FBlitter_32bppSSE4 iFBlitter_32bppSSE4;
+static FBlitter_32bppSSE4_Anim iFBlitter_32bppSSE4_Anim;
 
 #if defined(__GNUC__)
 	#pragma GCC diagnostic push
@@ -31,14 +31,15 @@ static FBlitter_32bppSSE4 iFBlitter_32bppSSE4;
  * @param zoom zoom level at which we are drawing
  */
 template <BlitterMode mode, Blitter_32bppSSE2::ReadMode read_mode, Blitter_32bppSSE2::BlockType bt_last>
-inline void Blitter_32bppSSE4::Draw(const Blitter::BlitterParams *bp, ZoomLevel zoom)
+inline void Blitter_32bppSSE4_Anim::Draw(const Blitter::BlitterParams *bp, ZoomLevel zoom)
 {
-	const byte * const remap = bp->remap;
 	Colour *dst_line = (Colour *) bp->dst + bp->top * bp->pitch + bp->left;
+	uint16 *anim_line = this->anim_buf + ((uint32 *)bp->dst - (uint32 *)_screen.dst_ptr) + bp->top * this->anim_buf_width + bp->left;
 	int effective_width = bp->width;
 
 	/* Find where to start reading in the source sprite. */
-	const SpriteData * const sd = (const SpriteData *) bp->sprite;
+	const byte * const remap = bp->remap;
+	const Blitter_32bppSSE_Base::SpriteData * const sd = (const Blitter_32bppSSE_Base::SpriteData *) bp->sprite;
 	const SpriteInfo * const si = &sd->infos[zoom];
 	const MapValue *src_mv_line = (const MapValue *) &sd->data[si->mv_offset] + bp->skip_top * si->sprite_width;
 	const Colour *src_rgba_line = (const Colour *) ((const byte *) &sd->data[si->sprite_offset] + bp->skip_top * si->sprite_line_size);
@@ -59,9 +60,10 @@ inline void Blitter_32bppSSE4::Draw(const Blitter::BlitterParams *bp, ZoomLevel 
 	const __m128i tr_nom_base = TRANSPARENT_NOM_BASE;
 
 	for (int y = bp->height; y != 0; y--) {
-		const Colour *src = src_rgba_line + META_LENGTH;
 		Colour *dst = dst_line;
+		const Colour *src = src_rgba_line + META_LENGTH;
 		const MapValue *src_mv = src_mv_line;
+		uint16 *anim = anim_line;
 
 		switch (mode) {
 			default: {
@@ -69,6 +71,8 @@ inline void Blitter_32bppSSE4::Draw(const Blitter::BlitterParams *bp, ZoomLevel 
 					case RM_WITH_MARGIN: {
 						src += src_rgba_line[0].data;
 						dst += src_rgba_line[0].data;
+						src_mv += src_rgba_line[0].data;
+						anim += src_rgba_line[0].data;
 						const int width_diff = si->sprite_width - bp->width;
 						effective_width = bp->width - (int) src_rgba_line[0].data;
 						const int delta_diff = (int) src_rgba_line[1].data - width_diff;
@@ -79,19 +83,79 @@ inline void Blitter_32bppSSE4::Draw(const Blitter::BlitterParams *bp, ZoomLevel 
 					}
 
 					case RM_WITH_SKIP: {
-						for (uint x = (uint) effective_width / 2; x > 0; x--) {
+						for (uint x = (uint) effective_width/2; x != 0; x--) {
+							uint32 mvX2 = *((uint32 *) const_cast<MapValue *>(src_mv));
 							__m128i srcABCD = _mm_loadl_epi64((const __m128i*) src);
 							__m128i dstABCD = _mm_loadl_epi64((__m128i*) dst);
+
+							/* Remap colours. */
+							const byte m0 = mvX2;
+							if (m0 >= PALETTE_ANIM_START) {
+								const Colour c0 = (this->LookupColourInPalette(m0).data & 0x00FFFFFF) | (src[0].data & 0xFF000000);
+								INSR32(AdjustBrightness(c0, (byte) (mvX2 >> 8)).data, srcABCD, 0);
+							}
+							const byte m1 = mvX2 >> 16;
+							if (m1 >= PALETTE_ANIM_START) {
+								const Colour c1 = (this->LookupColourInPalette(m1).data & 0x00FFFFFF) | (src[1].data & 0xFF000000);
+								INSR32(AdjustBrightness(c1, (byte) (mvX2 >> 24)).data, srcABCD, 1);
+							}
+
+							/* Update anim buffer. */
+							const byte a0 = src[0].a;
+							const byte a1 = src[1].a;
+							uint32 anim01 = 0;
+							if (a0 == 255) {
+								if (a1 == 255) {
+									*(uint32*) anim = mvX2;
+									goto bmno_full_opacity;
+								}
+								anim01 = (uint16) mvX2;
+							} else if (a0 == 0) {
+								if (a1 == 0) {
+									goto bmno_full_transparency;
+								} else {
+									if (a1 == 255) anim[1] = (uint16) (mvX2 >> 16);
+									goto bmno_alpha_blend;
+								}
+							}
+							if (a1 > 0) {
+								if (a1 == 255) anim01 |= mvX2 & 0xFFFF0000;
+								*(uint32*) anim = anim01;
+							} else {
+								anim[0] = (uint16) anim01;
+							}
+
+							/* Blend colours. */
+bmno_alpha_blend:
 							ALPHA_BLEND_2(pack_low_cm);
-							_mm_storel_epi64((__m128i*) dst, srcABCD);
+bmno_full_opacity:
+							_mm_storel_epi64((__m128i *) dst, srcABCD);
+bmno_full_transparency:
+							src_mv += 2;
 							src += 2;
+							anim += 2;
 							dst += 2;
 						}
+
 						if (bt_last == BT_ODD) {
-							__m128i srcABCD = _mm_cvtsi32_si128(src->data);
-							__m128i dstABCD = _mm_cvtsi32_si128(dst->data);
-							ALPHA_BLEND_2(pack_low_cm);
-							dst->data = _mm_cvtsi128_si32(srcABCD);
+							if (src->a == 0) {
+							} else if (src->a == 255) {
+								*anim = *(const uint16*) src_mv;
+								*dst = (src_mv->m >= PALETTE_ANIM_START) ? AdjustBrightness(LookupColourInPalette(src_mv->m), src_mv->v) : *src;
+							} else {
+								*anim = 0;
+								__m128i srcABCD;
+								__m128i dstABCD = _mm_cvtsi32_si128(dst->data);
+								if (src_mv->m >= PALETTE_ANIM_START) {
+									Colour colour = AdjustBrightness(LookupColourInPalette(src_mv->m), src_mv->v);
+									colour.a = src->a;
+									srcABCD = _mm_cvtsi32_si128(colour.data);
+								} else {
+									srcABCD = _mm_cvtsi32_si128(src->data);
+								}
+								ALPHA_BLEND_2(pack_low_cm);
+								dst->data = _mm_cvtsi128_si32(srcABCD);
+							}
 						}
 						break;
 					}
@@ -107,6 +171,7 @@ inline void Blitter_32bppSSE4::Draw(const Blitter::BlitterParams *bp, ZoomLevel 
 						src += src_rgba_line[0].data;
 						src_mv += src_rgba_line[0].data;
 						dst += src_rgba_line[0].data;
+						anim += src_rgba_line[0].data;
 						const int width_diff = si->sprite_width - bp->width;
 						effective_width = bp->width - (int) src_rgba_line[0].data;
 						const int delta_diff = (int) src_rgba_line[1].data - width_diff;
@@ -117,28 +182,28 @@ inline void Blitter_32bppSSE4::Draw(const Blitter::BlitterParams *bp, ZoomLevel 
 					}
 
 					case RM_WITH_SKIP: {
-						for (uint x = (uint) effective_width / 2; x > 0; x--) {
+						for (uint x = (uint) effective_width / 2; x != 0; x--) {
+							uint32 mvX2 = *((uint32 *) const_cast<MapValue *>(src_mv));
 							__m128i srcABCD = _mm_loadl_epi64((const __m128i*) src);
 							__m128i dstABCD = _mm_loadl_epi64((__m128i*) dst);
-							uint32 mvX2 = *((uint32 *) const_cast<MapValue *>(src_mv));
 
 							/* Remap colours. */
+							const uint m0 = (byte) mvX2;
+							const uint r0 = remap[m0];
+							const uint m1 = (byte) (mvX2 >> 16);
+							const uint r1 = remap[m1];
 							if (mvX2 & 0x00FF00FF) {
 								/* Written so the compiler uses CMOV. */
 								const Colour src0 = src[0];
-								const uint m0 = (byte) mvX2;
-								const uint r0 = remap[m0];
 								const Colour c0map = (this->LookupColourInPalette(r0).data & 0x00FFFFFF) | (src0.data & 0xFF000000);
-								Colour c0 = 0; // Use alpha of 0 to keep dst as is.
+								Colour c0 = dst[0];
 								c0 = r0 == 0 ? c0 : c0map;
 								c0 = m0 != 0 ? c0 : src0;
 								INSR32(c0.data, srcABCD, 0);
 
 								const Colour src1 = src[1];
-								const uint m1 = (byte) (mvX2 >> 16);
-								const uint r1 = remap[m1];
 								const Colour c1map = (this->LookupColourInPalette(r1).data & 0x00FFFFFF) | (src1.data & 0xFF000000);
-								Colour c1 = 0;
+								Colour c1 = dst[1];
 								c1 = r1 == 0 ? c1 : c1map;
 								c1 = m1 != 0 ? c1 : src1;
 								INSR32(c1.data, srcABCD, 1);
@@ -148,19 +213,52 @@ inline void Blitter_32bppSSE4::Draw(const Blitter::BlitterParams *bp, ZoomLevel 
 								}
 							}
 
+							/* Update anim buffer. */
+							const byte a0 = src[0].a;
+							const byte a1 = src[1].a;
+							uint32 anim01 = mvX2 & 0xFF00FF00;
+							if (a0 == 255) {
+								anim01 |= r0;
+								if (a1 == 255) {
+									*(uint32*) anim = anim01 | (r1 << 16);
+									goto bmcr_full_opacity;
+								}
+							} else if (a0 == 0) {
+								if (a1 == 0) {
+									goto bmcr_full_transparency;
+								} else {
+									if (a1 == 255) {
+										anim[1] = r1 | (anim01 >> 16);
+									}
+									goto bmcr_alpha_blend;
+								}
+							}
+							if (a1 > 0) {
+								if (a1 == 255) anim01 |= r1 << 16;
+								*(uint32*) anim = anim01;
+							} else {
+								anim[0] = (uint16) anim01;
+							}
+
 							/* Blend colours. */
+bmcr_alpha_blend:
 							ALPHA_BLEND_2(pack_low_cm);
+bmcr_full_opacity:
 							_mm_storel_epi64((__m128i *) dst, srcABCD);
+bmcr_full_transparency:
+							src_mv += 2;
 							dst += 2;
 							src += 2;
-							src_mv += 2;
+							anim += 2;
 						}
 
 						if (effective_width & 1) {
 							/* In case the m-channel is zero, do not remap this pixel in any way. */
 							__m128i srcABCD;
+							if (src->a == 0) break;
 							if (src_mv->m) {
 								const uint r = remap[src_mv->m];
+								*anim = (src->a == 255) ? r | ((uint16) src_mv->v << 8 ) : 0;
 								if (r != 0) {
 									Colour remapped_colour = AdjustBrightness(this->LookupColourInPalette(r), src_mv->v);
 									if (src->a == 255) {
@@ -172,6 +270,7 @@ inline void Blitter_32bppSSE4::Draw(const Blitter::BlitterParams *bp, ZoomLevel 
 									}
 								}
 							} else {
+								*anim = 0;
 								srcABCD = _mm_cvtsi32_si128(src->data);
 								if (src->a < 255) {
 bmcr_alpha_blend_single:
@@ -186,14 +285,11 @@ bmcr_alpha_blend_single:
 
 					default: NOT_REACHED();
 				}
-				src_mv_line += si->sprite_width;
 				break;
 			}
 
 			case BM_TRANSPARENT: {
-				/* Make the current colour a bit more black, so it looks like this image is transparent.
-				 * rgb = rgb * ((256/4) * 4 - (alpha/4)) / ((256/4) * 4)
-				 */
+				/* Make the current colour a bit more black, so it looks like this image is transparent. */
 				for (uint x = (uint) bp->width / 2; x > 0; x--) {
 					__m128i srcABCD = _mm_loadl_epi64((const __m128i*) src);
 					__m128i dstABCD = _mm_loadl_epi64((__m128i*) dst);
@@ -208,6 +304,9 @@ bmcr_alpha_blend_single:
 					_mm_storel_epi64((__m128i *) dst, dstAB);
 					src += 2;
 					dst += 2;
+					anim += 2;
+					if (src[-2].a) anim[-2] = 0;
+					if (src[-1].a) anim[-1] = 0;
 				}
 				if (bp->width & 1) {
 					__m128i srcABCD = _mm_cvtsi32_si128(src->data);
@@ -221,14 +320,16 @@ bmcr_alpha_blend_single:
 					dstAB = _mm_srli_epi16(dstAB, 8);
 					dstAB = _mm_packus_epi16(dstAB, dstAB);
 					dst->data = _mm_cvtsi128_si32(dstAB);
+					if (src[0].a) anim[0] = 0;
 				}
-
 				break;
 			}
 		}
 
+		src_mv_line += si->sprite_width;
 		src_rgba_line = (const Colour*) ((const byte*) src_rgba_line + si->sprite_line_size);
 		dst_line += bp->pitch;
+		anim_line += this->anim_buf_width;
 	}
 }
 #if defined(__GNUC__)
@@ -242,11 +343,11 @@ bmcr_alpha_blend_single:
  * @param mode blitter mode
  * @param zoom zoom level at which we are drawing
  */
-void Blitter_32bppSSE4::Draw(Blitter::BlitterParams *bp, BlitterMode mode, ZoomLevel zoom)
+void Blitter_32bppSSE4_Anim::Draw(Blitter::BlitterParams *bp, BlitterMode mode, ZoomLevel zoom)
 {
-	const BlockType bt_last = (BlockType) (bp->width & 1);
 	switch (mode) {
 		case BM_NORMAL: {
+			const BlockType bt_last = (BlockType) (bp->width & 1);
 			if (bp->skip_left != 0 || bp->width <= MARGIN_NORMAL_THRESHOLD) {
 				switch (bt_last) {
 					case BT_EVEN: Draw<BM_NORMAL, RM_WITH_SKIP, BT_EVEN>(bp, zoom); return;
@@ -274,44 +375,12 @@ void Blitter_32bppSSE4::Draw(Blitter::BlitterParams *bp, BlitterMode mode, ZoomL
 }
 
 /** Same code as seen in 32bpp_sse2.cpp but some macros are not the same. */
-inline Colour Blitter_32bppSSE4::AdjustBrightness(Colour colour, uint8 brightness)
+inline Colour Blitter_32bppSSE4_Anim::AdjustBrightness(Colour colour, uint8 brightness)
 {
 	/* Shortcut for normal brightness. */
 	if (brightness == DEFAULT_BRIGHTNESS) return colour;
 
 	return Blitter_32bppSSE4::ReallyAdjustBrightness(colour, brightness);
-}
-
-/* static */ Colour Blitter_32bppSSE4::ReallyAdjustBrightness(Colour colour, uint8 brightness)
-{
-	uint64 c16 = colour.b | (uint64) colour.g << 16 | (uint64) colour.r << 32;
-	c16 *= brightness;
-	uint64 c16_ob = c16; // Helps out of order execution.
-	c16 /= DEFAULT_BRIGHTNESS;
-	c16 &= 0x01FF01FF01FFULL;
-
-	/* Sum overbright (maximum for each rgb is 508, 9 bits, -255 is changed in -256 so we just have to take the 8 lower bits into account). */
-	c16_ob = (((c16_ob >> (8 + 7)) & 0x0100010001ULL) * 0xFF) & c16;
-	uint64 ob = (uint16) c16_ob + (uint16) (c16_ob >> 16) + (uint16) (c16_ob >> 32);
-
-	const uint32 alpha32 = colour.data & 0xFF000000;
-	__m128i ret;
-	INSR64(c16, ret, 0);
-	if (ob != 0) {
-		/* Reduce overbright strength. */
-		ob /= 2;
-		__m128i ob128;
-		INSR64(ob | ob << 16 | ob << 32, ob128, 0);
-		__m128i white = OVERBRIGHT_VALUE_MASK;
-		__m128i c128 = ret;
-		ret = _mm_subs_epu16(white, c128); /* PSUBUSW,   (255 - rgb) */
-		ret = _mm_mullo_epi16(ret, ob128); /* PMULLW, ob*(255 - rgb) */
-		ret = _mm_srli_epi16(ret, 8);      /* PSRLW,  ob*(255 - rgb)/256 */
-		ret = _mm_add_epi16(ret, c128);    /* PADDW,  ob*(255 - rgb)/256 + rgb */
-	}
-
-	ret = _mm_packus_epi16(ret, ret);      /* PACKUSWB, saturate and pack. */
-	return alpha32 | _mm_cvtsi128_si32(ret);
 }
 
 #endif /* WITH_SSE */
