yahho/OpenTTD-patches

commit 37759e52358e413db83f4b4edef1bf527f2c1fa6

Add signal methods to RailPathPos
Add methods in RailPathPos to query signal existence, type and
state.

src/pathfinder/railpos.h
@@ -34,6 +34,56 @@ struct RailPathPos : PathPos<PathVTile> {
 		return !in_wormhole() ? GetRailType (tile, TrackdirToTrack(td)) :
 			IsRailwayTile(wormhole) ? GetBridgeRailType(wormhole) : GetRailType(wormhole);
 	}
+
+	/** Check if there are signals at a position */
+	bool has_signals() const
+	{
+		if (in_wormhole()) {
+			return false;
+		} else if (IsRailwayTile(tile)) {
+			return HasSignalOnTrack (tile, TrackdirToTrack(td));
+		} else if (maptile_is_rail_tunnel(tile)) {
+			return maptile_has_tunnel_signals (tile);
+		} else {
+			return false;
+		}
+	}
+
+	/** Get the type of signals at a position */
+	SignalType get_signal_type() const
+	{
+		assert(has_signals());
+		return IsRailwayTile(tile) ?
+			GetSignalType (tile, TrackdirToTrack(td)) :
+			maptile_get_tunnel_signal_type(tile);
+	}
+
+	/** Check if there is a signal along/against a position */
+	bool has_signal_along (bool along = true) const
+	{
+		if (in_wormhole()) {
+			return false;
+		} else if (IsRailwayTile(tile)) {
+			return HasSignalOnTrackdir (tile, along ? td : ReverseTrackdir(td));
+		} else if (maptile_is_rail_tunnel(tile)) {
+			bool inwards = TrackdirToExitdir(td) == GetTunnelBridgeDirection(tile);
+			return maptile_has_tunnel_signal (tile, along ^ inwards);
+		} else {
+			return false;
+		}
+	}
+
+	/** Check if there is a signal against a position */
+	bool has_signal_against() const { return has_signal_along(false); }
+
+	/** Get the state of the signal along a position */
+	SignalState get_signal_state() const
+	{
+		assert(has_signal_along());
+		return IsRailwayTile(tile) ?
+			GetSignalStateByTrackdir (tile, td) :
+			maptile_get_tunnel_signal_state (tile, TrackdirToExitdir(td) == GetTunnelBridgeDirection(tile));
+	}
 };
 
 #endif /* PATHFINDER_RAILPOS_H */

src/pathfinder/yapf/yapf_rail.cpp
@@ -464,9 +464,9 @@ inline int CYapfRailBaseT<TAstar>::SignalCost(Node& n, const RailPathPos &pos, N
 	/* if there is one-way signal in the opposite direction, then it is not our way */
 	CPerfStart perf_cost(m_perf_other_cost);
 
-	if (HasSignalAlongPos(pos)) {
-		SignalState sig_state = GetSignalStateByPos(pos);
-		SignalType sig_type = GetSignalType(pos);
+	if (pos.has_signal_along()) {
+		SignalState sig_state = pos.get_signal_state();
+		SignalType sig_type = pos.get_signal_type();
 
 		n.m_last_signal_type = sig_type;
 
@@ -487,7 +487,7 @@ inline int CYapfRailBaseT<TAstar>::SignalCost(Node& n, const RailPathPos &pos, N
 					&& m_settings->rail_firstred_twoway_eol
 					&& m_treat_first_red_two_way_signal_as_eol
 					&& n.flags.test(n.FLAG_CHOICE_SEEN)
-					&& HasSignalAgainstPos(pos)
+					&& pos.has_signal_against()
 					&& n.m_num_signals_passed == 0) {
 				/* yes, the first signal is two-way red signal => DEAD END. Prune this branch... */
 				PruneIntermediateNodeBranch();
@@ -518,8 +518,8 @@ inline int CYapfRailBaseT<TAstar>::SignalCost(Node& n, const RailPathPos &pos, N
 
 		n.m_num_signals_passed++;
 		data->last_signal = pos;
-	} else if (HasSignalAgainstPos(pos)) {
-		if (GetSignalType(pos) != SIGTYPE_PBS) {
+	} else if (pos.has_signal_against()) {
+		if (pos.get_signal_type() != SIGTYPE_PBS) {
 			/* one-way signal in opposite direction */
 			data->end_reason |= ESRB_DEAD_END;
 		} else {
@@ -614,7 +614,7 @@ inline void CYapfRailBaseT<TAstar>::HandleNodeTile (Node *n, const CFollowTrackR
 
 	} else if (mask_reserved_tracks) {
 		/* Searching for a safe tile? */
-		if (HasSignalAlongPos(segment->pos) && !IsPbsSignal(GetSignalType(segment->pos))) {
+		if (segment->pos.has_signal_along() && !IsPbsSignal(segment->pos.get_signal_type())) {
 			segment->end_reason |= ESRB_SAFE_TILE;
 		}
 	}
@@ -670,10 +670,10 @@ inline bool CYapfRailBaseT<TAstar>::HandleNodeNextTile (Node *n, CFollowTrackRai
 	/* Gather the next tile/trackdir. */
 
 	if (mask_reserved_tracks) {
-		if (HasSignalAlongPos(tf->m_new) && IsPbsSignal(GetSignalType(tf->m_new))) {
+		if (tf->m_new.has_signal_along() && IsPbsSignal(tf->m_new.get_signal_type())) {
 			/* Possible safe tile. */
 			segment->end_reason |= ESRB_SAFE_TILE;
-		} else if (HasSignalAgainstPos(tf->m_new) && GetSignalType(tf->m_new) == SIGTYPE_PBS_ONEWAY) {
+		} else if (tf->m_new.has_signal_against() && tf->m_new.get_signal_type() == SIGTYPE_PBS_ONEWAY) {
 			/* Possible safe tile, but not so good as it's the back of a signal... */
 			segment->end_reason |= ESRB_SAFE_TILE | ESRB_DEAD_END;
 			segment->extra_cost += m_settings->rail_lastred_exit_penalty;
@@ -791,12 +791,12 @@ inline EndSegmentReasonBits CYapfRailBaseT<TAstar>::RestoreCachedNode (Node *n)
 	n->m_cost = n->m_parent->m_cost + TransitionCost (n->m_parent->GetLastPos(), n->GetPos()) + n->m_segment->m_cost;
 	/* We will need also some information about the last signal (if it was red). */
 	if (n->m_segment->m_last_signal.is_valid_tile()) {
-		assert(HasSignalAlongPos(n->m_segment->m_last_signal));
-		SignalState sig_state = GetSignalStateByPos(n->m_segment->m_last_signal);
+		assert(n->m_segment->m_last_signal.has_signal_along());
+		SignalState sig_state = n->m_segment->m_last_signal.get_signal_state();
 		bool is_red = (sig_state == SIGNAL_STATE_RED);
 		n->flags.set (n->FLAG_LAST_SIGNAL_WAS_RED, is_red);
 		if (is_red) {
-			n->m_last_red_signal_type = GetSignalType(n->m_segment->m_last_signal);
+			n->m_last_red_signal_type = n->m_segment->m_last_signal.get_signal_type();
 		}
 	}
 	/* No further calculation needed. */

src/pbs.cpp
@@ -261,7 +261,7 @@ static RailPathPos FollowReservation(Owner o, RailTypes rts, const RailPathPos &
 		/* Depot tile? Can't continue. */
 		if (!cur.in_wormhole() && IsRailDepotTile(cur.tile)) break;
 		/* Non-pbs signal? Reservation can't continue. */
-		if (HasSignalAlongPos(cur) && !IsPbsSignal(GetSignalType(cur))) break;
+		if (cur.has_signal_along() && !IsPbsSignal(cur.get_signal_type())) break;
 	}
 
 	return cur;
@@ -426,7 +426,7 @@ PBSPositionState CheckWaitingPosition(const Train *v, const RailPathPos &pos, bo
 		/* Depots are always safe, and free iff unreserved. */
 		if (IsRailDepotTile(pos.tile) && pos.td == DiagDirToDiagTrackdir(ReverseDiagDir(GetGroundDepotDirection(pos.tile)))) return HasDepotReservation(pos.tile) ? PBS_BUSY : PBS_FREE;
 
-		if (HasSignalAlongPos(pos) && !IsPbsSignal(GetSignalType(pos))) {
+		if (pos.has_signal_along() && !IsPbsSignal(pos.get_signal_type())) {
 			/* For non-pbs signals, stop on the signal tile. */
 			if (cb == PBS_CHECK_SAFE) return PBS_FREE;
 			return HasReservedTrack(pos.tile, TrackdirToTrack(pos.td)) ? PBS_BUSY : PBS_FREE;
@@ -454,12 +454,12 @@ PBSPositionState CheckWaitingPosition(const Train *v, const RailPathPos &pos, bo
 	if (cb != PBS_CHECK_FREE) {
 		if (!ft.m_new.is_single()) return PBS_UNSAFE;
 
-		if (HasSignalAlongPos(ft.m_new)) {
+		if (ft.m_new.has_signal_along()) {
 			/* PBS signal on next trackdir? Safe position. */
-			if (!IsPbsSignal(GetSignalType(ft.m_new))) return PBS_UNSAFE;
-		} else if (HasSignalAgainstPos(ft.m_new)) {
+			if (!IsPbsSignal(ft.m_new.get_signal_type())) return PBS_UNSAFE;
+		} else if (ft.m_new.has_signal_against()) {
 			/* One-way PBS signal against us? Safe position. */
-			if (GetSignalType(ft.m_new) != SIGTYPE_PBS_ONEWAY) return PBS_UNSAFE;
+			if (ft.m_new.get_signal_type() != SIGTYPE_PBS_ONEWAY) return PBS_UNSAFE;
 		} else {
 			/* No signal at all? Unsafe position. */
 			return PBS_UNSAFE;
@@ -470,8 +470,8 @@ PBSPositionState CheckWaitingPosition(const Train *v, const RailPathPos &pos, bo
 	} else if (!IsStationTile(pos.tile)) {
 		/* With PBS_CHECK_FREE, all these should be true. */
 		assert(ft.m_new.is_single());
-		assert(HasSignalOnPos(ft.m_new));
-		assert(IsPbsSignal(GetSignalType(ft.m_new)));
+		assert(ft.m_new.has_signals());
+		assert(IsPbsSignal(ft.m_new.get_signal_type()));
 	}
 
 	assert(state == PBS_FREE);

src/signal_map.h
@@ -31,70 +31,6 @@ static inline void SetSignalState(TileIndex tile, Trackdir trackdir, SignalState
 
 
 /**
- * Checks for the presence of signals along the given trackdir.
- */
-static inline bool HasSignalAlongPos(const RailPathPos &pos)
-{
-	if (pos.in_wormhole()) {
-		return false;
-	} else if (IsRailwayTile(pos.tile)) {
-		return HasSignalOnTrackdir(pos.tile, pos.td);
-	} else if (maptile_is_rail_tunnel(pos.tile)) {
-		return maptile_has_tunnel_signal(pos.tile, TrackdirToExitdir(pos.td) == GetTunnelBridgeDirection(pos.tile));
-	} else {
-		return false;
-	}
-}
-
-/**
- * Checks for the presence of signals against the given trackdir.
- */
-static inline bool HasSignalAgainstPos(const RailPathPos &pos)
-{
-	if (pos.in_wormhole()) {
-		return false;
-	} else if (IsRailwayTile(pos.tile)) {
-		return HasSignalOnTrackdir(pos.tile, ReverseTrackdir(pos.td));
-	} else if (maptile_is_rail_tunnel(pos.tile)) {
-		return maptile_has_tunnel_signal(pos.tile, TrackdirToExitdir(pos.td) != GetTunnelBridgeDirection(pos.tile));
-	} else {
-		return false;
-	}
-}
-
-/**
- * Checks for the presence of signals along or against the given trackdir.
- */
-static inline bool HasSignalOnPos(const RailPathPos &pos)
-{
-	if (pos.in_wormhole()) {
-		return false;
-	} else if (IsRailwayTile(pos.tile)) {
-		return HasSignalOnTrack(pos.tile, TrackdirToTrack(pos.td));
-	} else if (maptile_is_rail_tunnel(pos.tile)) {
-		return maptile_has_tunnel_signals(pos.tile);
-	} else {
-		return false;
-	}
-}
-
-static inline SignalType GetSignalType(const RailPathPos &pos)
-{
-	assert(HasSignalOnPos(pos));
-	return IsRailwayTile(pos.tile) ? GetSignalType(pos.tile, TrackdirToTrack(pos.td)) : maptile_get_tunnel_signal_type(pos.tile);
-}
-
-/**
- * Gets the state of the signal along the given trackdir.
- */
-static inline SignalState GetSignalStateByPos(const RailPathPos &pos)
-{
-	return IsRailwayTile(pos.tile) ? GetSignalStateByTrackdir(pos.tile, pos.td) :
-		maptile_get_tunnel_signal_state(pos.tile, TrackdirToExitdir(pos.td) == GetTunnelBridgeDirection(pos.tile));
-}
-
-
-/**
  * Is a pbs signal present along the trackdir?
  * @param tile the tile to check
  * @param td the trackdir to check

src/train_cmd.cpp
@@ -1913,8 +1913,8 @@ void ReverseTrainDirection(Train *v)
 
 		/* If we are currently on a tile with conventional signals, we can't treat the
 		 * current tile as a safe tile or we would enter a PBS block without a reservation. */
-		bool first_tile_okay = !(HasSignalAlongPos(pos) &&
-			!IsPbsSignal(GetSignalType(pos)));
+		bool first_tile_okay = !(pos.has_signal_along() &&
+			!IsPbsSignal(pos.get_signal_type()));
 
 		/* If we are on a depot tile facing outwards, do not treat the current tile as safe. */
 		if (!pos.in_wormhole() && IsRailDepotTile(pos.tile) && TrackdirToExitdir(pos.td) == GetGroundDepotDirection(pos.tile)) first_tile_okay = false;
@@ -2143,9 +2143,9 @@ static void CheckNextTrainTile(Train *v)
 	RailPathPos pos = v->GetPos();
 
 	/* On a tile with a red non-pbs signal, don't look ahead. */
-	if (HasSignalAlongPos(pos) &&
-			!IsPbsSignal(GetSignalType(pos)) &&
-			GetSignalStateByPos(pos) == SIGNAL_STATE_RED) return;
+	if (pos.has_signal_along() &&
+			!IsPbsSignal(pos.get_signal_type()) &&
+			pos.get_signal_state() == SIGNAL_STATE_RED) return;
 
 	CFollowTrackRail ft(v, !_settings_game.pf.forbid_90_deg);
 	if (!ft.Follow(pos)) return;
@@ -2317,14 +2317,14 @@ void FreeTrainTrackReservation(const Train *v)
 			assert(ft.m_new.is_single());
 		}
 
-		if (HasSignalAlongPos(ft.m_new) && !IsPbsSignal(GetSignalType(ft.m_new))) {
+		if (ft.m_new.has_signal_along() && !IsPbsSignal(ft.m_new.get_signal_type())) {
 			/* Conventional signal along trackdir: remove reservation and stop. */
 			UnreserveRailTrack(ft.m_new);
 			break;
 		}
 
 		if (HasPbsSignalAlongPos(ft.m_new)) {
-			if (GetSignalStateByPos(ft.m_new) == SIGNAL_STATE_RED) {
+			if (ft.m_new.get_signal_state() == SIGNAL_STATE_RED) {
 				/* Red PBS signal? Can't be our reservation, would be green then. */
 				break;
 			} else {
@@ -2332,7 +2332,7 @@ void FreeTrainTrackReservation(const Train *v)
 				SetSignalState(ft.m_new.tile, ft.m_new.td, SIGNAL_STATE_RED);
 				MarkTileDirtyByTile(ft.m_new.tile);
 			}
-		} else if (HasSignalAgainstPos(ft.m_new) && IsOnewaySignal(GetSignalType(ft.m_new))) {
+		} else if (ft.m_new.has_signal_against() && IsOnewaySignal(ft.m_new.get_signal_type())) {
 			break;
 		}
 
@@ -3630,7 +3630,7 @@ bool TrainController(Train *v, Vehicle *nomove, bool reverse)
 				ClearPathReservation(v, v->GetPos());
 
 				RailPathPos rev = v->GetReversePos();
-				if (HasSignalOnPos(rev)) {
+				if (rev.has_signals()) {
 					assert(IsSignalBufferEmpty());
 					AddPosToSignalBuffer(rev, v->owner);
 					/* Defer actual updating of signals until the train has moved */
@@ -3735,14 +3735,14 @@ bool TrainController(Train *v, Vehicle *nomove, bool reverse)
 
 			if (v->IsFrontEngine()) {
 				RailPathPos pos = v->GetPos();
-				if (HasSignalOnPos(pos)) {
+				if (pos.has_signals()) {
 					assert(IsSignalBufferEmpty());
 					AddPosToSignalBuffer(pos, v->owner);
 
 					if (UpdateSignalsInBuffer() == SIGSEG_PBS &&
-							HasSignalAlongPos(pos) &&
+							pos.has_signal_along() &&
 							/* A PBS block with a non-PBS signal facing us? */
-							!IsPbsSignal(GetSignalType(pos))) {
+							!IsPbsSignal(pos.get_signal_type())) {
 						/* We are entering a block with PBS signals right now, but
 						 * not through a PBS signal. This means we don't have a
 						 * reservation right now. As a conventional signal will only

commit e9cedf1c9800f285ed0cff7b72d090f6ae1d9de4

Fix check for tunnel head signals in RailPathPos
Commit 37759e52358e413db83f4b4edef1bf527f2c1fa6 introduced a bug
when defining RailPathPos::has_signal_along in that the logic
to check for a signal along/against a tunnel head position was
actually inverted, leading to assertions. Fix this.

src/pathfinder/railpos.h
@@ -67,7 +67,7 @@ struct RailPathPos : PathPos<PathVTile> {
 			return HasSignalOnTrackdir (tile, along ? td : ReverseTrackdir(td));
 		} else if (maptile_is_rail_tunnel(tile)) {
 			bool inwards = TrackdirToExitdir(td) == GetTunnelBridgeDirection(tile);
-			return maptile_has_tunnel_signal (tile, along ^ inwards);
+			return maptile_has_tunnel_signal (tile, along == inwards);
 		} else {
 			return false;
 		}