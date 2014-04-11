bf9daacf4c664dcaa22f00818601cdd4a00a1f40
cirdan <cirdansw@gmail.com>
2014-04-11 22:32:26 +0200

    Remove CYapfRailBaseT::NodeData
    It was helpful at the time to reorganise the code in CalcSegment,
    but it has now outlived its usefulness.

--- src/pathfinder/yapf/yapf_rail.cpp
+++ src/pathfinder/yapf/yapf_rail.cpp
@@ -439,14 +439,6 @@ public:
 		m_max_cost = max_cost;
 	}
 
-	struct NodeData {
-		int segment_cost;
-		int extra_cost;
-		RailPathPos pos;
-		RailPathPos last_signal;
-		EndSegmentReasonBits end_reason;
-	};
-
 	/** Return the transition cost from one tile to another. */
 	inline int TransitionCost (const RailPathPos &pos1, const RailPathPos &pos2) const
 	{
@@ -561,13 +553,13 @@ public:
 	}
 
 	/* Compute cost and modify node state for a signal. */
-	inline int SignalCost(Node *n, const RailPathPos &pos, NodeData *data);
+	inline int SignalCost(Node *n, const RailPathPos &pos);
 
 	/* Compute cost and modify node state for a position. */
-	inline void HandleNodeTile (Node *n, const CFollowTrackRail *tf, NodeData *segment, TileIndex prev);
+	inline void HandleNodeTile (Node *n, const CFollowTrackRail *tf, TileIndex prev);
 
 	/* Check for possible reasons to end a segment at the next tile. */
-	inline void HandleNodeNextTile (Node *n, CFollowTrackRail *tf, NodeData *segment, RailType rail_type);
+	inline void HandleNodeNextTile (Node *n, CFollowTrackRail *tf, RailType rail_type);
 
 	/** Compute all costs for a newly-allocated (not cached) segment. */
 	inline bool CalcSegment (Node *n, const CFollowTrackRail *tf);
@@ -600,7 +592,7 @@ public:
 
 /** Compute cost and modify node state for a signal. */
 template <class TAstar>
-inline int CYapfRailBaseT<TAstar>::SignalCost(Node *n, const RailPathPos &pos, NodeData *data)
+inline int CYapfRailBaseT<TAstar>::SignalCost(Node *n, const RailPathPos &pos)
 {
 	int cost = 0;
 	/* if there is one-way signal in the opposite direction, then it is not our way */
@@ -632,7 +624,7 @@ inline int CYapfRailBaseT<TAstar>::SignalCost(Node *n, const RailPathPos &pos, N
 					&& pos.has_signal_against()
 					&& n->m_num_signals_passed == 0) {
 				/* yes, the first signal is two-way red signal => DEAD END */
-				data->end_reason |= ESRB_DEAD_END;
+				n->m_segment->m_end_segment_reason |= ESRB_DEAD_END;
 				m_stopped_on_first_two_way_signal = true;
 				/* prune this branch, so that we will not follow a best
 				 * intermediate node that heads straight into this one */
@@ -664,11 +656,11 @@ inline int CYapfRailBaseT<TAstar>::SignalCost(Node *n, const RailPathPos &pos, N
 		}
 
 		n->m_num_signals_passed++;
-		data->last_signal = pos;
+		n->m_segment->m_last_signal = pos;
 	} else if (pos.has_signal_against()) {
 		if (pos.get_signal_type() != SIGTYPE_PBS) {
 			/* one-way signal in opposite direction */
-			data->end_reason |= ESRB_DEAD_END;
+			n->m_segment->m_end_segment_reason |= ESRB_DEAD_END;
 		} else {
 			cost += n->m_num_signals_passed < m_settings->rail_look_ahead_max_signals ? m_settings->rail_pbs_signal_back_penalty : 0;
 		}
@@ -679,50 +671,53 @@ inline int CYapfRailBaseT<TAstar>::SignalCost(Node *n, const RailPathPos &pos, N
 
 /** Compute cost and modify node state for a position. */
 template <class TAstar>
-inline void CYapfRailBaseT<TAstar>::HandleNodeTile (Node *n, const CFollowTrackRail *tf, NodeData *segment, TileIndex prev)
+inline void CYapfRailBaseT<TAstar>::HandleNodeTile (Node *n, const CFollowTrackRail *tf, TileIndex prev)
 {
+	const RailPathPos &pos = n->m_segment->m_last;
+	int cost = 0;
+
 	/* All other tile costs will be calculated here. */
-	segment->segment_cost += OneTileCost(segment->pos);
+	cost += OneTileCost(pos);
 
 	/* If we skipped some tunnel/bridge/station tiles, add their base cost */
-	segment->segment_cost += YAPF_TILE_LENGTH * tf->m_tiles_skipped;
+	cost += YAPF_TILE_LENGTH * tf->m_tiles_skipped;
 
 	/* Slope cost. */
-	segment->segment_cost += SlopeCost(segment->pos);
+	cost += SlopeCost(pos);
 
 	/* Signal cost (routine can modify segment data). */
-	segment->segment_cost += SignalCost(n, segment->pos, segment);
+	cost += SignalCost(n, pos);
 
 	/* Reserved tiles. */
-	segment->segment_cost += ReservationCost(*n, segment->pos, tf->m_tiles_skipped);
+	cost += ReservationCost(*n, pos, tf->m_tiles_skipped);
 
 	/* Tests for 'potential target' reasons to close the segment. */
-	if (segment->pos.tile == prev) {
+	if (pos.tile == prev) {
 		/* Penalty for reversing in a depot. */
-		assert(!segment->pos.in_wormhole());
-		assert(IsRailDepotTile(segment->pos.tile));
-		assert(segment->pos.td == DiagDirToDiagTrackdir(GetGroundDepotDirection(segment->pos.tile)));
-		segment->segment_cost += m_settings->rail_depot_reverse_penalty;
+		assert(!pos.in_wormhole());
+		assert(IsRailDepotTile(pos.tile));
+		assert(pos.td == DiagDirToDiagTrackdir(GetGroundDepotDirection(pos.tile)));
+		cost += m_settings->rail_depot_reverse_penalty;
 		/* We will end in this pass (depot is possible target) */
-		segment->end_reason |= ESRB_DEPOT;
+		n->m_segment->m_end_segment_reason |= ESRB_DEPOT;
 
-	} else if (!segment->pos.in_wormhole() && IsRailWaypointTile(segment->pos.tile)) {
+	} else if (!pos.in_wormhole() && IsRailWaypointTile(pos.tile)) {
 		/* Waypoint is also a good reason to finish. */
-		segment->end_reason |= ESRB_WAYPOINT;
+		n->m_segment->m_end_segment_reason |= ESRB_WAYPOINT;
 
 	} else if (tf->m_flag == tf->TF_STATION) {
 		/* Station penalties. */
 		uint platform_length = tf->m_tiles_skipped + 1;
 		/* We don't know yet if the station is our target or not. Act like
 		 * if it is pass-through station (not our destination). */
-		segment->segment_cost += m_settings->rail_station_penalty * platform_length;
+		cost += m_settings->rail_station_penalty * platform_length;
 		/* We will end in this pass (station is possible target) */
-		segment->end_reason |= ESRB_STATION;
+		n->m_segment->m_end_segment_reason |= ESRB_STATION;
 
 	} else if (mask_reserved_tracks) {
 		/* Searching for a safe tile? */
-		if (segment->pos.has_signal_along() && !IsPbsSignal(segment->pos.get_signal_type())) {
-			segment->end_reason |= ESRB_SAFE_TILE;
+		if (pos.has_signal_along() && !IsPbsSignal(pos.get_signal_type())) {
+			n->m_segment->m_end_segment_reason |= ESRB_SAFE_TILE;
 		}
 	}
 
@@ -733,26 +728,28 @@ inline void CYapfRailBaseT<TAstar>::HandleNodeTile (Node *n, const CFollowTrackR
 		int max_speed = tf->GetSpeedLimit();
 		int max_veh_speed = m_veh->GetDisplayMaxSpeed();
 		if (max_speed < max_veh_speed) {
-			segment->extra_cost += YAPF_TILE_LENGTH * (max_veh_speed - max_speed) * (1 + tf->m_tiles_skipped) / max_veh_speed;
+			cost += YAPF_TILE_LENGTH * (max_veh_speed - max_speed) * (1 + tf->m_tiles_skipped) / max_veh_speed;
 		}
 	}
+
+	n->m_segment->m_cost += cost;
 }
 
 /** Check for possible reasons to end a segment at the next tile. */
 template <class TAstar>
-inline void CYapfRailBaseT<TAstar>::HandleNodeNextTile (Node *n, CFollowTrackRail *tf, NodeData *segment, RailType rail_type)
+inline void CYapfRailBaseT<TAstar>::HandleNodeNextTile (Node *n, CFollowTrackRail *tf, RailType rail_type)
 {
-	if (!tf->Follow(segment->pos)) {
+	if (!tf->Follow(n->m_segment->m_last)) {
 		assert(tf->m_err != CFollowTrackRail::EC_NONE);
 		/* Can't move to the next tile (EOL?). */
 		if (tf->m_err == CFollowTrackRail::EC_RAIL_TYPE) {
-			segment->end_reason |= ESRB_RAIL_TYPE;
+			n->m_segment->m_end_segment_reason |= ESRB_RAIL_TYPE;
 		} else {
-			segment->end_reason |= ESRB_DEAD_END;
+			n->m_segment->m_end_segment_reason |= ESRB_DEAD_END;
 		}
 
-		if (mask_reserved_tracks && !HasOnewaySignalBlockingPos(segment->pos)) {
-			segment->end_reason |= ESRB_SAFE_TILE;
+		if (mask_reserved_tracks && !HasOnewaySignalBlockingPos(tf->m_old)) {
+			n->m_segment->m_end_segment_reason |= ESRB_SAFE_TILE;
 		}
 		return;
 	}
@@ -760,7 +757,7 @@ inline void CYapfRailBaseT<TAstar>::HandleNodeNextTile (Node *n, CFollowTrackRai
 	/* Check if the next tile is not a choice. */
 	if (!tf->m_new.is_single()) {
 		/* More than one segment will follow. Close this one. */
-		segment->end_reason |= ESRB_CHOICE_FOLLOWS;
+		n->m_segment->m_end_segment_reason |= ESRB_CHOICE_FOLLOWS;
 		return;
 	}
 
@@ -769,11 +766,11 @@ inline void CYapfRailBaseT<TAstar>::HandleNodeNextTile (Node *n, CFollowTrackRai
 	if (mask_reserved_tracks) {
 		if (tf->m_new.has_signal_along() && IsPbsSignal(tf->m_new.get_signal_type())) {
 			/* Possible safe tile. */
-			segment->end_reason |= ESRB_SAFE_TILE;
+			n->m_segment->m_end_segment_reason |= ESRB_SAFE_TILE;
 		} else if (tf->m_new.has_signal_against() && tf->m_new.get_signal_type() == SIGTYPE_PBS_ONEWAY) {
 			/* Possible safe tile, but not so good as it's the back of a signal... */
-			segment->end_reason |= ESRB_SAFE_TILE | ESRB_DEAD_END;
-			segment->extra_cost += m_settings->rail_lastred_exit_penalty;
+			n->m_segment->m_end_segment_reason |= ESRB_SAFE_TILE | ESRB_DEAD_END;
+			n->m_segment->m_cost += m_settings->rail_lastred_exit_penalty;
 		}
 	}
 
@@ -783,18 +780,18 @@ inline void CYapfRailBaseT<TAstar>::HandleNodeNextTile (Node *n, CFollowTrackRai
 		assert(next_rail_type == rail_type);
 	} else if (tf->m_new.get_railtype() != rail_type) {
 		/* Segment must consist from the same rail_type tiles. */
-		segment->end_reason |= ESRB_RAIL_TYPE;
+		n->m_segment->m_end_segment_reason |= ESRB_RAIL_TYPE;
 		return;
 	}
 
 	/* Avoid infinite looping. */
 	if (tf->m_new == n->GetPos()) {
-		segment->end_reason |= ESRB_INFINITE_LOOP;
-	} else if (segment->segment_cost > s_max_segment_cost) {
+		n->m_segment->m_end_segment_reason |= ESRB_INFINITE_LOOP;
+	} else if (n->m_segment->m_cost > s_max_segment_cost) {
 		/* Potentially in the infinite loop (or only very long segment?). We should
 		 * not force it to finish prematurely unless we are on a regular tile. */
 		if (!tf->m_new.in_wormhole() && IsNormalRailTile(tf->m_new.tile)) {
-			segment->end_reason |= ESRB_SEGMENT_TOO_LONG;
+			n->m_segment->m_end_segment_reason |= ESRB_SEGMENT_TOO_LONG;
 		}
 	}
 }
@@ -829,23 +826,22 @@ inline bool CYapfRailBaseT<TAstar>::CalcSegment (Node *n, const CFollowTrackRail
 	 * cost of segment entry (move from the 'parent' node) is not included!
 	 */
 
+	assert (n->m_segment->m_last == n->GetPos());
+	assert (n->m_segment->m_cost == 0);
+	assert (!n->m_segment->m_last_signal.is_valid_tile());
+	assert (n->m_segment->m_end_segment_reason == ESRB_NONE);
+
 	/* start at n and walk to the end of segment */
 	int entry_cost = n->m_parent->m_cost + TransitionCost (n->m_parent->GetLastPos(), n->GetPos());
-	NodeData segment;
-	segment.segment_cost = 0;
-	segment.extra_cost = 0;
-	segment.pos = n->GetPos();
-	segment.last_signal = RailPathPos();
-	segment.end_reason = ESRB_NONE;
 
 	TileIndex prev = n->m_parent->GetLastPos().tile;
 
-	RailType rail_type = segment.pos.get_railtype();
+	RailType rail_type = n->GetPos().get_railtype();
 
 	bool path_too_long;
 
 	for (;;) {
-		HandleNodeTile (n, tf, &segment, prev);
+		HandleNodeTile (n, tf, prev);
 
 		/* Move to the next tile/trackdir. */
 		tf = &tf_local;
@@ -853,31 +849,26 @@ inline bool CYapfRailBaseT<TAstar>::CalcSegment (Node *n, const CFollowTrackRail
 		assert(tf_local.m_railtypes == m_compatible_railtypes);
 		assert(tf_local.m_pPerf == &m_perf_ts_cost);
 
-		HandleNodeNextTile (n, &tf_local, &segment, rail_type);
+		HandleNodeNextTile (n, &tf_local, rail_type);
+		assert(tf_local.m_old == n->m_segment->m_last);
 
 		/* Finish if we already exceeded the maximum path cost
 		 * (i.e. when searching for the nearest depot). */
-		path_too_long = m_max_cost > 0 && (entry_cost + segment.segment_cost) > m_max_cost;
+		path_too_long = m_max_cost > 0 && (entry_cost + n->m_segment->m_cost) > m_max_cost;
 
 		/* Any reason to end the segment? */
-		if (path_too_long || (segment.end_reason != ESRB_NONE)) break;
+		if (path_too_long || (n->m_segment->m_end_segment_reason != ESRB_NONE)) break;
 
 		/* Transition cost (cost of the move from previous tile) */
-		segment.segment_cost += TransitionCost (segment.pos, tf_local.m_new);
+		n->m_segment->m_cost += TransitionCost (tf_local.m_old, tf_local.m_new);
 
 		/* For the next loop set new prev and cur tile info. */
-		prev = segment.pos.tile;
-		segment.pos = tf_local.m_new;
+		prev = tf_local.m_old.tile;
+		n->m_segment->m_last = tf_local.m_new;
 	} // for (;;)
 
-	/* Write back the segment information so it can be reused the next time. */
-	n->m_segment->m_last = segment.pos;
-	n->m_segment->m_cost = segment.segment_cost;
-	n->m_segment->m_last_signal = segment.last_signal;
-	n->m_segment->m_end_segment_reason = segment.end_reason;
-
 	/* total node cost */
-	n->m_cost = entry_cost + segment.segment_cost + segment.extra_cost;
+	n->m_cost = entry_cost + n->m_segment->m_cost;
 	return path_too_long;
 }
 
