20fe7115ada042744699b479c531a7627c16c99c
cirdan <cirdansw@gmail.com>
2014-05-15 13:50:54 +0200

    Check colour first when deciding whether to draw a shadow
    DrawLayoutLine draws a shadow for a stretch of text if the font
    says so and the text colour is not black. Check the colour first,
    as checking the font goes through a virtual method and is
    therefore more expensive.

--- src/gfx.cpp
+++ src/gfx.cpp
@@ -432,7 +432,7 @@ static int DrawLayoutLine(const ParagraphLayouter::Line *line, int y, int left, 
 		int dpi_left  = dpi->left;
 		int dpi_right = dpi->left + dpi->width - 1;
 
-		draw_shadow = fc->GetDrawGlyphShadow() && colour != TC_BLACK;
+		draw_shadow = colour != TC_BLACK && fc->GetDrawGlyphShadow();
 
 		for (int i = 0; i < run->GetGlyphCount(); i++) {
 			GlyphID glyph = run->GetGlyphs()[i];
