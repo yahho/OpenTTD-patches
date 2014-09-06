82f515c7f9bbfebce7c60899fd97f45b88b9cd85
rubidium <rubidium@openttd.org>
2014-09-06 20:11:00 +0000

    Remove unused SQUIRREL_API macro
    (openttd r26780) -Cleanup: remove unused SQUIRREL_API macro

--- src/3rdparty/squirrel/include/sqstdstring.h
+++ src/3rdparty/squirrel/include/sqstdstring.h
@@ -10,16 +10,16 @@ typedef struct {
 	SQInteger len;
 } SQRexMatch;
 
-SQUIRREL_API SQRex *sqstd_rex_compile(const char *pattern,const char **error);
-SQUIRREL_API void sqstd_rex_free(SQRex *exp);
-SQUIRREL_API SQBool sqstd_rex_match(SQRex* exp,const char* text);
-SQUIRREL_API SQBool sqstd_rex_search(SQRex* exp,const char* text, const char** out_begin, const char** out_end);
-SQUIRREL_API SQBool sqstd_rex_searchrange(SQRex* exp,const char* text_begin,const char* text_end,const char** out_begin, const char** out_end);
-SQUIRREL_API SQInteger sqstd_rex_getsubexpcount(SQRex* exp);
-SQUIRREL_API SQBool sqstd_rex_getsubexp(SQRex* exp, SQInteger n, SQRexMatch *subexp);
+SQRex *sqstd_rex_compile(const char *pattern,const char **error);
+void sqstd_rex_free(SQRex *exp);
+SQBool sqstd_rex_match(SQRex* exp,const char* text);
+SQBool sqstd_rex_search(SQRex* exp,const char* text, const char** out_begin, const char** out_end);
+SQBool sqstd_rex_searchrange(SQRex* exp,const char* text_begin,const char* text_end,const char** out_begin, const char** out_end);
+SQInteger sqstd_rex_getsubexpcount(SQRex* exp);
+SQBool sqstd_rex_getsubexp(SQRex* exp, SQInteger n, SQRexMatch *subexp);
 
-SQUIRREL_API SQRESULT sqstd_format(HSQUIRRELVM v,SQInteger nformatstringidx,SQInteger *outlen,char **output);
+SQRESULT sqstd_format(HSQUIRRELVM v,SQInteger nformatstringidx,SQInteger *outlen,char **output);
 
-SQUIRREL_API SQRESULT sqstd_register_stringlib(HSQUIRRELVM v);
+SQRESULT sqstd_register_stringlib(HSQUIRRELVM v);
 
 #endif /*_SQSTD_STRING_H_*/
