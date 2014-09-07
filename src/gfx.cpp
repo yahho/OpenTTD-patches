41a9d1f3ba1f28dae0e714ebf737d60a11c1dd59
frosch <frosch@openttd.org>
2014-09-07 15:25:54 +0000

    Fix TC_NO_SHADE with 32bpp text rendering
    (openttd r26792) -Fix: TC_NO_SHADE did not work for 32bpp text
    rendering.

--- src/gfx.cpp
+++ src/gfx.cpp
@@ -433,7 +433,7 @@ static int DrawLayoutLine(const ParagraphLayouter::Line *line, int y, int left, 
 		int dpi_left  = dpi->left;
 		int dpi_right = dpi->left + dpi->width - 1;
 
-		draw_shadow = colour != TC_BLACK && fc->GetDrawGlyphShadow();
+		draw_shadow = colour != TC_BLACK && (colour & TC_NO_SHADE) == 0 && fc->GetDrawGlyphShadow();
 
 		for (int i = 0; i < run->GetGlyphCount(); i++) {
 			GlyphID glyph = run->GetGlyphs()[i];
