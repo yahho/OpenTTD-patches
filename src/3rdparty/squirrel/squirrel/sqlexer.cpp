1568b33ace58768a9d8c0b4c43edd96a68e7d268
rubidium <rubidium@openttd.org>
2014-09-16 20:07:47 +0000

    Remove some dead code from the squirrel lexer
    (openttd r26835) -Fix [Squirrel]: remove dead code from the lexer

--- src/3rdparty/squirrel/squirrel/sqlexer.cpp
+++ src/3rdparty/squirrel/squirrel/sqlexer.cpp
@@ -152,11 +152,9 @@ SQInteger SQLexer::Lex()
 			case '=':
 				NEXT();
 				RETURN_TOKEN(TK_DIVEQ);
-				continue;
 			case '>':
 				NEXT();
 				RETURN_TOKEN(TK_ATTR_CLOSE);
-				continue;
 			default:
 				RETURN_TOKEN('/');
 			}
