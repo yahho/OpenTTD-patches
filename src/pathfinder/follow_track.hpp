yahho/OpenTTD-patches

commit 0ee5d007e149f2a894e717b2e0912ad217ad7ccd

Fix value returned in m_err in the track follower
The track follower was almost always returning EC_NO_WAY as error
condition on failure. This is because, aside from two conditions
directly detected by Follow (when CheckOldTile returns TR_NO_WAY
and when advancing into the next tile would make a 90-degree turn
and that is forbidden), the only two other places that set m_err
were CheckNewTile and CheckEndOfLine, but any value set for m_err
by CheckNewTile would be immediately overwritten by CheckEndOfLine,
which always set m_err to EC_NO_WAY. Fix this by ensuring that
m_err is set to some error code whenever CheckNewTile fails and
that CheckEndOfLine leaves m_err alone. While at it, also reset
m_err to EC_NONE if CheckNewTile fails but CheckEndOfLine succeeds.

src/pathfinder/follow_track.hpp
@@ -149,7 +149,10 @@ struct CFollowTrackRailBase : CFollowTrackBase
 		}
 
 		m_new.trackdirs &= DiagdirReachesTrackdirs(m_exitdir);
-		if (m_new.trackdirs == TRACKDIR_BIT_NONE) return false;
+		if (m_new.trackdirs == TRACKDIR_BIT_NONE) {
+			m_err = EC_NO_WAY;
+			return false;
+		}
 
 		perf.Stop();
 
@@ -220,7 +223,6 @@ struct CFollowTrackRailBase : CFollowTrackBase
 	/** return true if we successfully reversed at end of road/track */
 	inline bool CheckEndOfLine()
 	{
-		m_err = EC_NO_WAY;
 		return false;
 	}
 
@@ -456,12 +458,9 @@ struct CFollowTrackRoadBase : CFollowTrackBase
 		m_new.trackdirs = GetTrackStatusTrackdirBits(m_new.tile);
 
 		if (m_new.trackdirs == TRACKDIR_BIT_NONE) {
-			if (!IsTram()) return false;
-
 			/* GetTileRoadStatus() returns 0 for single tram bits.
 			 * As we cannot change it there (easily) without breaking something, change it here */
-			DiagDirection single_tram = GetSingleTramBit(m_new.tile);
-			if (single_tram == ReverseDiagDir(m_exitdir)) {
+			if (IsTram() && GetSingleTramBit(m_new.tile) == ReverseDiagDir(m_exitdir)) {
 				m_new.trackdirs = TrackdirToTrackdirBits(DiagDirToDiagTrackdir(m_exitdir));
 				return true;
 			} else {
@@ -471,7 +470,10 @@ struct CFollowTrackRoadBase : CFollowTrackBase
 		}
 
 		m_new.trackdirs &= DiagdirReachesTrackdirs(m_exitdir);
-		if (m_new.trackdirs == TRACKDIR_BIT_NONE) return false;
+		if (m_new.trackdirs == TRACKDIR_BIT_NONE) {
+			m_err = EC_NO_WAY;
+			return false;
+		}
 
 		if (IsStandardRoadStopTile(m_new.tile)) {
 			/* road stop can be entered from one direction only unless it's a drive-through stop */
@@ -543,7 +545,6 @@ struct CFollowTrackRoadBase : CFollowTrackBase
 			m_new.SetTrackdir();
 			return true;
 		}
-		m_err = EC_NO_WAY;
 		return false;
 	}
 
@@ -610,7 +611,10 @@ struct CFollowTrackWaterBase : CFollowTrackBase
 	inline bool CheckNewTile()
 	{
 		m_new.trackdirs = GetTrackStatusTrackdirBits(m_new.tile) & DiagdirReachesTrackdirs(m_exitdir);
-		if (m_new.trackdirs == TRACKDIR_BIT_NONE) return false;
+		if (m_new.trackdirs == TRACKDIR_BIT_NONE) {
+			m_err = EC_NO_WAY;
+			return false;
+		}
 
 		/* tunnel holes and bridge ramps can be entered only from proper direction */
 		assert(m_flag == TF_NONE);
@@ -626,7 +630,6 @@ struct CFollowTrackWaterBase : CFollowTrackBase
 	/** return true if we successfully reversed at end of road/track */
 	inline bool CheckEndOfLine()
 	{
-		m_err = EC_NO_WAY;
 		return false;
 	}
 
@@ -735,7 +738,10 @@ struct CFollowTrack : Base
 		}
 
 		if (!Base::CheckNewTile()) {
-			return Base::CheckEndOfLine();
+			assert(Base::m_err != Base::EC_NONE);
+			if (!Base::CheckEndOfLine()) return false;
+			Base::m_err = Base::EC_NONE; // clear error set by CheckNewTile
+			return false;
 		}
 
 		if (!Base::Allow90deg()) {

commit 86957e50989c5564a2284f23f151c04888ffb1bf

Restore reversing of road vehicles at end of line
Commit 0ee5d007e149f2a894e717b2e0912ad217ad7ccd introduced a bug
in CFollowTrack::Follow when relaying the return value from
CheckEndOfLine, and always set it to false. This effectively
prevented road vehicles from considering reversing at end of line.

src/pathfinder/follow_track.hpp
@@ -174,7 +174,7 @@ struct CFollowTrack : Base
 			assert(Base::m_err != Base::EC_NONE);
 			if (!Base::CheckEndOfLine()) return false;
 			Base::m_err = Base::EC_NONE; // clear error set by CheckNewTile
-			return false;
+			return true;
 		}
 
 		if (!Base::Allow90deg()) {