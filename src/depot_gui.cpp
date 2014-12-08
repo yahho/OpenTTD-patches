yahho/OpenTTD-patches

commit b3c2883f294903a286ec6dfc8fa80fea6940d960

Convert the strings subsystem to use stringb
Convert the strings subsystem (GetString, GetStringWithArgs and
related functions) to use stringb.

src/depot_gui.cpp
@@ -813,9 +813,7 @@ struct DepotWindow : Window {
 		}
 
 		/* Build tooltipstring */
-		static char details[1024];
-		details[0] = '\0';
-		char *pos = details;
+		sstring<1024> details;
 
 		for (CargoID cargo_type = 0; cargo_type < NUM_CARGO; cargo_type++) {
 			if (capacity[cargo_type] == 0) continue;
@@ -824,13 +822,13 @@ struct DepotWindow : Window {
 			SetDParam(1, loaded[cargo_type]);   // {CARGO} #2
 			SetDParam(2, cargo_type);           // {SHORTCARGO} #1
 			SetDParam(3, capacity[cargo_type]); // {SHORTCARGO} #2
-			pos = GetString(pos, STR_DEPOT_VEHICLE_TOOLTIP_CARGO, lastof(details));
+			AppendString (&details, STR_DEPOT_VEHICLE_TOOLTIP_CARGO);
 		}
 
 		/* Show tooltip window */
 		uint64 args[2];
 		args[0] = (whole_chain ? num : v->engine_type);
-		args[1] = (uint64)(size_t)details;
+		args[1] = (uint64)(size_t)details.c_str();
 		GuiShowTooltips(this, whole_chain ? STR_DEPOT_VEHICLE_TOOLTIP_CHAIN : STR_DEPOT_VEHICLE_TOOLTIP, 2, args, TCC_RIGHT_CLICK);
 
 		return true;

src/industry_gui.cpp
@@ -68,11 +68,10 @@ static void ShowIndustryCargoesWindow(IndustryType id);
  * @param ind_type the industry type
  * @param indspec the industry spec
  * @param suffix is filled with the string to display
- * @param suffix_last lastof(suffix)
  */
-static void GetCargoSuffix(uint cargo, CargoSuffixType cst, const Industry *ind, IndustryType ind_type, const IndustrySpec *indspec, char *suffix, const char *suffix_last)
+static void GetCargoSuffix (uint cargo, CargoSuffixType cst, const Industry *ind, IndustryType ind_type, const IndustrySpec *indspec, stringb *suffix)
 {
-	suffix[0] = '\0';
+	suffix->clear();
 	if (HasBit(indspec->callback_mask, CBM_IND_CARGO_SUFFIX)) {
 		uint16 callback = GetIndustryCallback(CBID_INDUSTRY_CARGO_SUFFIX, 0, (cst << 8) | cargo, const_cast<Industry *>(ind), ind_type, (cst != CST_FUND) ? ind->location.tile : INVALID_TILE);
 		if (callback == CALLBACK_FAILED || callback == 0x400) return;
@@ -80,7 +79,7 @@ static void GetCargoSuffix(uint cargo, CargoSuffixType cst, const Industry *ind,
 			ErrorUnknownCallbackResult(indspec->grf_prop.grffile->grfid, CBID_INDUSTRY_CARGO_SUFFIX, callback);
 		} else if (indspec->grf_prop.grffile->grf_version >= 8 || GB(callback, 0, 8) != 0xFF) {
 			StartTextRefStackUsage(indspec->grf_prop.grffile, 6);
-			GetString(suffix, GetGRFStringID(indspec->grf_prop.grffile->grfid, 0xD000 + callback), suffix_last);
+			GetString (suffix, GetGRFStringID(indspec->grf_prop.grffile->grfid, 0xD000 + callback));
 			StopTextRefStackUsage();
 		}
 	}
@@ -102,9 +101,9 @@ static inline void GetAllCargoSuffixes(uint cb_offset, CargoSuffixType cst, cons
 	assert_compile(lengthof(cargoes) <= lengthof(suffixes));
 	for (uint j = 0; j < lengthof(cargoes); j++) {
 		if (cargoes[j] != CT_INVALID) {
-			GetCargoSuffix(cb_offset + j, cst, ind, ind_type, indspec, suffixes[j], lastof(suffixes[j]));
+			GetCargoSuffix (cb_offset + j, cst, ind, ind_type, indspec, &suffixes[j]);
 		} else {
-			suffixes[j][0] = '\0';
+			suffixes[j].clear();
 		}
 	}
 }
@@ -312,7 +311,7 @@ public:
 
 					const IndustrySpec *indsp = GetIndustrySpec(this->index[i]);
 
-					char cargo_suffix[3][512];
+					sstring<512> cargo_suffix [3];
 					GetAllCargoSuffixes(0, CST_FUND, NULL, this->index[i], indsp, indsp->accepts_cargo, cargo_suffix);
 					StringID str = STR_INDUSTRY_VIEW_REQUIRES_CARGO;
 					byte p = 0;
@@ -322,7 +321,7 @@ public:
 						if (indsp->accepts_cargo[j] == CT_INVALID) continue;
 						if (p > 0) str++;
 						SetDParam(p++, CargoSpec::Get(indsp->accepts_cargo[j])->name);
-						SetDParamStr(p++, cargo_suffix[j]);
+						SetDParamStr(p++, cargo_suffix[j].c_str());
 					}
 					d = maxdim(d, GetStringBoundingBox(str));
 
@@ -336,7 +335,7 @@ public:
 						if (indsp->produced_cargo[j] == CT_INVALID) continue;
 						if (p > 0) str++;
 						SetDParam(p++, CargoSpec::Get(indsp->produced_cargo[j])->name);
-						SetDParamStr(p++, cargo_suffix[j]);
+						SetDParamStr(p++, cargo_suffix[j].c_str());
 					}
 					d = maxdim(d, GetStringBoundingBox(str));
 				}
@@ -431,7 +430,7 @@ public:
 				}
 
 				/* Draw the accepted cargoes, if any. Otherwise, will print "Nothing". */
-				char cargo_suffix[3][512];
+				sstring<512> cargo_suffix [3];
 				GetAllCargoSuffixes(0, CST_FUND, NULL, this->selected_type, indsp, indsp->accepts_cargo, cargo_suffix);
 				StringID str = STR_INDUSTRY_VIEW_REQUIRES_CARGO;
 				byte p = 0;
@@ -441,7 +440,7 @@ public:
 					if (indsp->accepts_cargo[j] == CT_INVALID) continue;
 					if (p > 0) str++;
 					SetDParam(p++, CargoSpec::Get(indsp->accepts_cargo[j])->name);
-					SetDParamStr(p++, cargo_suffix[j]);
+					SetDParamStr(p++, cargo_suffix[j].c_str());
 				}
 				DrawString(left, right, y, str);
 				y += FONT_HEIGHT_NORMAL;
@@ -456,7 +455,7 @@ public:
 					if (indsp->produced_cargo[j] == CT_INVALID) continue;
 					if (p > 0) str++;
 					SetDParam(p++, CargoSpec::Get(indsp->produced_cargo[j])->name);
-					SetDParamStr(p++, cargo_suffix[j]);
+					SetDParamStr(p++, cargo_suffix[j].c_str());
 				}
 				DrawString(left, right, y, str);
 				y += FONT_HEIGHT_NORMAL;
@@ -711,7 +710,7 @@ public:
 		int y = top + WD_FRAMERECT_TOP;
 		bool first = true;
 		bool has_accept = false;
-		char cargo_suffix[3][512];
+		sstring<512> cargo_suffix [3];
 
 		if (i->prod_level == PRODLEVEL_CLOSURE) {
 			DrawString(left + WD_FRAMERECT_LEFT, right - WD_FRAMERECT_RIGHT, y, STR_INDUSTRY_VIEW_INDUSTRY_ANNOUNCED_CLOSURE);
@@ -730,7 +729,7 @@ public:
 				}
 				SetDParam(0, i->accepts_cargo[j]);
 				SetDParam(1, i->incoming_cargo_waiting[j]);
-				SetDParamStr(2, cargo_suffix[j]);
+				SetDParamStr(2, cargo_suffix[j].c_str());
 				DrawString(left + WD_FRAMETEXT_LEFT, right - WD_FRAMERECT_RIGHT, y, STR_INDUSTRY_VIEW_WAITING_STOCKPILE_CARGO);
 				y += FONT_HEIGHT_NORMAL;
 			}
@@ -743,7 +742,7 @@ public:
 				has_accept = true;
 				if (p > 0) str++;
 				SetDParam(p++, CargoSpec::Get(i->accepts_cargo[j])->name);
-				SetDParamStr(p++, cargo_suffix[j]);
+				SetDParamStr(p++, cargo_suffix[j].c_str());
 			}
 			if (has_accept) {
 				DrawString(left + WD_FRAMERECT_LEFT, right - WD_FRAMERECT_RIGHT, y, str);
@@ -765,7 +764,7 @@ public:
 
 			SetDParam(0, i->produced_cargo[j]);
 			SetDParam(1, i->last_month_production[j]);
-			SetDParamStr(2, cargo_suffix[j]);
+			SetDParamStr(2, cargo_suffix[j].c_str());
 			SetDParam(3, ToPercent8(i->last_month_pct_transported[j]));
 			uint x = left + WD_FRAMETEXT_LEFT + (this->editable == EA_RATE ? SETTING_BUTTON_WIDTH + 10 : 0);
 			DrawString(x, right - WD_FRAMERECT_RIGHT, y, STR_INDUSTRY_VIEW_TRANSPORTED);
@@ -1202,7 +1201,7 @@ protected:
 		/* Industry name */
 		SetDParam(p++, i->index);
 
-		static char cargo_suffix[lengthof(i->produced_cargo)][512];
+		sstring<512> cargo_suffix [lengthof(i->produced_cargo)];
 		GetAllCargoSuffixes(3, CST_DIR, i, i->type, indsp, i->produced_cargo, cargo_suffix);
 
 		/* Industry productions */
@@ -1210,7 +1209,7 @@ protected:
 			if (i->produced_cargo[j] == CT_INVALID) continue;
 			SetDParam(p++, i->produced_cargo[j]);
 			SetDParam(p++, i->last_month_production[j]);
-			SetDParamStr(p++, cargo_suffix[j]);
+			SetDParamStr(p++, cargo_suffix[j].c_str());
 		}
 
 		/* Transported productions */

src/error_gui.cpp
@@ -371,21 +371,21 @@ void ShowErrorMessage(StringID summary_msg, StringID detailed_msg, WarningLevel 
 
 	if (wl != WL_INFO) {
 		/* Print message to console */
-		char buf[DRAW_STRING_BUFFER];
+		sstring<DRAW_STRING_BUFFER> buf;
 
 		if (textref_stack_size > 0) StartTextRefStackUsage(textref_stack_grffile, textref_stack_size, textref_stack);
 
-		char *b = GetString(buf, summary_msg, lastof(buf));
+		GetString (&buf, summary_msg);
 		if (detailed_msg != INVALID_STRING_ID) {
-			b += seprintf(b, lastof(buf), " ");
-			GetString(b, detailed_msg, lastof(buf));
+			buf.append (' ');
+			AppendString (&buf, detailed_msg);
 		}
 
 		if (textref_stack_size > 0) StopTextRefStackUsage();
 
 		switch (wl) {
-			case WL_WARNING: IConsolePrint(CC_WARNING, buf); break;
-			default:         IConsoleError(buf); break;
+			case WL_WARNING: IConsolePrint(CC_WARNING, buf.c_str()); break;
+			default:         IConsoleError(buf.c_str()); break;
 		}
 	}
 

src/fios_gui.cpp
@@ -242,8 +242,7 @@ public:
 	/** Generate a default save filename. */
 	void GenerateFileName()
 	{
-		GenerateDefaultSaveName(this->filename_editbox.buffer, &this->filename_editbox.buffer[this->filename_editbox.capacity - 1]);
-		this->filename_editbox.len = strlen (this->filename_editbox.c_str());
+		GenerateDefaultSaveName (&this->filename_editbox);
 		this->filename_editbox.UpdateSize();
 	}
 

src/misc_gui.cpp
@@ -63,7 +63,7 @@ class LandInfoWindow : public Window {
 
 public:
 	char landinfo_data[LAND_INFO_CENTERED_LINES][LAND_INFO_LINE_BUFF_SIZE];
-	char landinfo_multicenter[LAND_INFO_LINE_BUFF_SIZE];
+	sstring<LAND_INFO_LINE_BUFF_SIZE> landinfo_multicenter;
 	TileIndex tile;
 
 	virtual void DrawWidget(const Rect &r, int widget) const
@@ -79,8 +79,8 @@ public:
 			if (i == 0) y += 4;
 		}
 
-		if (!StrEmpty(this->landinfo_multicenter)) {
-			SetDParamStr(0, this->landinfo_multicenter);
+		if (!this->landinfo_multicenter.empty()) {
+			SetDParamStr(0, this->landinfo_multicenter.c_str());
 			DrawStringMultiLine(r.left + WD_FRAMETEXT_LEFT, r.right - WD_FRAMETEXT_RIGHT, y, r.bottom - WD_TEXTPANEL_BOTTOM, STR_JUST_RAW_STRING, TC_FROMSTRING, SA_CENTER);
 		}
 	}
@@ -100,10 +100,10 @@ public:
 			if (i == 0) size->height += 4;
 		}
 
-		if (!StrEmpty(this->landinfo_multicenter)) {
-			uint width = GetStringBoundingBox(this->landinfo_multicenter).width + WD_FRAMETEXT_LEFT + WD_FRAMETEXT_RIGHT;
+		if (!this->landinfo_multicenter.empty()) {
+			uint width = GetStringBoundingBox(this->landinfo_multicenter.c_str()).width + WD_FRAMETEXT_LEFT + WD_FRAMETEXT_RIGHT;
 			size->width = max(size->width, min(300u, width));
-			SetDParamStr(0, this->landinfo_multicenter);
+			SetDParamStr(0, this->landinfo_multicenter.c_str());
 			size->height += GetStringHeight(STR_JUST_RAW_STRING, size->width - WD_FRAMETEXT_LEFT - WD_FRAMETEXT_RIGHT);
 		}
 	}
@@ -279,29 +279,26 @@ public:
 		this->landinfo_data[line_nr][0] = '\0';
 
 		/* Cargo acceptance is displayed in a extra multiline */
-		char *strp = GetString(this->landinfo_multicenter, STR_LAND_AREA_INFORMATION_CARGO_ACCEPTED, lastof(this->landinfo_multicenter));
+		GetString (&this->landinfo_multicenter, STR_LAND_AREA_INFORMATION_CARGO_ACCEPTED);
 		bool found = false;
 
 		for (CargoID i = 0; i < NUM_CARGO; ++i) {
 			if (acceptance[i] > 0) {
 				/* Add a comma between each item. */
-				if (found) {
-					*strp++ = ',';
-					*strp++ = ' ';
-				}
+				if (found) this->landinfo_multicenter.append (", ");
 				found = true;
 
 				/* If the accepted value is less than 8, show it in 1/8:ths */
 				if (acceptance[i] < 8) {
 					SetDParam(0, acceptance[i]);
 					SetDParam(1, CargoSpec::Get(i)->name);
-					strp = GetString(strp, STR_LAND_AREA_INFORMATION_CARGO_EIGHTS, lastof(this->landinfo_multicenter));
+					AppendString (&this->landinfo_multicenter, STR_LAND_AREA_INFORMATION_CARGO_EIGHTS);
 				} else {
-					strp = GetString(strp, CargoSpec::Get(i)->name, lastof(this->landinfo_multicenter));
+					AppendString (&this->landinfo_multicenter, CargoSpec::Get(i)->name);
 				}
 			}
 		}
-		if (!found) this->landinfo_multicenter[0] = '\0';
+		if (!found) this->landinfo_multicenter.clear();
 	}
 
 	virtual bool IsNewGRFInspectable() const
@@ -912,10 +909,8 @@ struct QueryStringWindow : public Window
 	QueryStringWindow(StringID str, StringID caption, uint max_bytes, uint max_chars, WindowDesc *desc, Window *parent, CharSetFilter afilter, QueryStringFlags flags) :
 			Window(desc), editbox(max_bytes, max_chars)
 	{
-		char *last_of = &this->editbox.buffer[this->editbox.capacity - 1];
-		GetString(this->editbox.buffer, str, last_of);
-		this->editbox.len = strlen (this->editbox.c_str());
-		str_validate(this->editbox.buffer, last_of, SVS_NONE);
+		GetString (&this->editbox, str);
+		this->editbox.validate (SVS_NONE);
 
 		/* Make sure the name isn't too long for the text buffer in the number of
 		 * characters (not bytes). max_chars also counts the '\0' characters. */

src/network/network.cpp
@@ -264,7 +264,7 @@ void NetworkTextMessage(NetworkAction action, TextColour colour, bool self_send,
 		default:                            strid = STR_NETWORK_CHAT_ALL; break;
 	}
 
-	char message[1024];
+	sstring<1024> message;
 	SetDParamStr(0, name);
 	SetDParamStr(1, str);
 	SetDParam(2, data);
@@ -273,12 +273,12 @@ void NetworkTextMessage(NetworkAction action, TextColour colour, bool self_send,
 	 * right-to-left characters depending on the context. As the next text might be an user's name, the
 	 * user name's characters will influence the direction of the "***" instead of the language setting
 	 * of the game. Manually set the direction of the "***" by inserting a text-direction marker. */
-	char *msg_ptr = message + Utf8Encode(message, _current_text_dir == TD_LTR ? CHAR_TD_LRM : CHAR_TD_RLM);
-	GetString(msg_ptr, strid, lastof(message));
+	message.append_utf8 (_current_text_dir == TD_LTR ? CHAR_TD_LRM : CHAR_TD_RLM);
+	AppendString (&message, strid);
 
-	DEBUG(desync, 1, "msg: %08x; %02x; %s", _date, _date_fract, message);
-	IConsolePrintF(colour, "%s", message);
-	NetworkAddChatMessage((TextColour)colour, _settings_client.gui.network_chat_timeout, "%s", message);
+	DEBUG(desync, 1, "msg: %08x; %02x; %s", _date, _date_fract, message.c_str());
+	IConsolePrintF(colour, "%s", message.c_str());
+	NetworkAddChatMessage((TextColour)colour, _settings_client.gui.network_chat_timeout, "%s", message.c_str());
 }
 
 /* Calculate the frame-lag of a client */

src/network/network_server.cpp
@@ -1512,12 +1512,14 @@ NetworkRecvStatus ServerNetworkGameSocketHandler::Receive_CLIENT_MOVE(Packet *p)
  */
 void NetworkSocketHandler::SendCompanyInformation(Packet *p, const Company *c, const NetworkCompanyStats *stats, uint max_len)
 {
-	/* Grab the company name */
-	char company_name[NETWORK_COMPANY_NAME_LENGTH];
-	SetDParam(0, c->index);
+	assert (max_len > 0);
+	assert (max_len <= NETWORK_COMPANY_NAME_LENGTH);
 
-	assert(max_len <= lengthof(company_name));
-	GetString(company_name, STR_COMPANY_NAME, company_name + max_len - 1);
+	/* Grab the company name */
+	sstring<NETWORK_COMPANY_NAME_LENGTH> company_name;
+	SetDParam(0, c->index);
+	GetString (&company_name, STR_COMPANY_NAME);
+	if (company_name.length() >= max_len) company_name.truncate (max_len - 1);
 
 	/* Get the income */
 	Money income = 0;
@@ -1534,7 +1536,7 @@ void NetworkSocketHandler::SendCompanyInformation(Packet *p, const Company *c, c
 
 	/* Send the information */
 	p->Send_uint8 (c->index);
-	p->Send_string(company_name);
+	p->Send_string(company_name.c_str());
 	p->Send_uint32(c->inaugurated_year);
 	p->Send_uint64(c->old_economy[0].company_value);
 	p->Send_uint64(c->money);

src/network/network_udp.cpp
@@ -228,10 +228,10 @@ void ServerNetworkUDPSocketHandler::Receive_CLIENT_DETAIL_INFO(Packet *p, Networ
 		size_t name_lengths [MAX_COMPANIES];
 		Company *company;
 		FOR_ALL_COMPANIES(company) {
-			char company_name[NETWORK_COMPANY_NAME_LENGTH];
+			sstring<NETWORK_COMPANY_NAME_LENGTH> company_name;
 			SetDParam(0, company->index);
-			GetString(company_name, STR_COMPANY_NAME, lastof(company_name));
-			name_lengths[company->index] = strlen (company_name);
+			GetString (&company_name, STR_COMPANY_NAME);
+			name_lengths[company->index] = company_name.length();
 		}
 
 		for (;;) {

commit 037561755b8255c4595499a33c6905158bbb25d6

Restore static qualifier to two string buffers
Commit b3c2883f294903a286ec6dfc8fa80fea6940d960 accidentally
removed the static qualifier from two string buffers, and this
was causing crashes when accessing their contents after they
had gone out of scope.

src/depot_gui.cpp
@@ -826,7 +826,8 @@ struct DepotWindow : Window {
 		}
 
 		/* Build tooltipstring */
-		sstring<1024> details;
+		static char details [1024];
+		stringb sb (details);
 
 		for (CargoID cargo_type = 0; cargo_type < NUM_CARGO; cargo_type++) {
 			if (capacity[cargo_type] == 0) continue;
@@ -835,13 +836,13 @@ struct DepotWindow : Window {
 			SetDParam(1, loaded[cargo_type]);   // {CARGO} #2
 			SetDParam(2, cargo_type);           // {SHORTCARGO} #1
 			SetDParam(3, capacity[cargo_type]); // {SHORTCARGO} #2
-			AppendString (&details, STR_DEPOT_VEHICLE_TOOLTIP_CARGO);
+			AppendString (&sb, STR_DEPOT_VEHICLE_TOOLTIP_CARGO);
 		}
 
 		/* Show tooltip window */
 		uint64 args[2];
 		args[0] = (whole_chain ? num : v->engine_type);
-		args[1] = (uint64)(size_t)details.c_str();
+		args[1] = (uint64)(size_t)details;
 		GuiShowTooltips(this, whole_chain ? STR_DEPOT_VEHICLE_TOOLTIP_CHAIN : STR_DEPOT_VEHICLE_TOOLTIP, 2, args, TCC_RIGHT_CLICK);
 
 		return true;

src/industry_gui.cpp
@@ -1202,7 +1202,7 @@ protected:
 		/* Industry name */
 		SetDParam(p++, i->index);
 
-		sstring<512> cargo_suffix [lengthof(i->produced_cargo)];
+		static sstring<512> cargo_suffix [lengthof(i->produced_cargo)];
 		GetAllCargoSuffixes(3, CST_DIR, i, i->type, indsp, i->produced_cargo, cargo_suffix);
 
 		/* Industry productions */