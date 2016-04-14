943206d0e579d5906fa647f773a65c2fe7a4782f
cirdan <cirdansw@gmail.com>
2016-04-14 16:55:43 +0200

    Move _name_swedish_*
    Move _name_swedish_* into MakeSwedishTownName.

--- src/townnamegen.cpp
+++ src/townnamegen.cpp
@@ -560,19 +560,59 @@ static void MakeSillyTownName (stringb *buf, uint32 seed)
  */
 static void MakeSwedishTownName (stringb *buf, uint32 seed)
 {
+	static const char * const names_1[] = {
+		"Gamla ", "Lilla ", "Nya ", "Stora ",
+	};
+
+	static const char * const names_2[] = {
+		"Boll", "Bor", "Ed", "En", "Erik", "Es", "Fin", "Fisk",
+		"Gr\xC3\xB6n", "Hag", "Halm", "Karl", "Kram", "Kung", "Land",
+		"Lid", "Lin", "Mal", "Malm", "Marie", "Ner", "Norr", "Oskar",
+		"Sand", "Skog", "Stock", "Stor", "Str\xC3\xB6m", "Sund",
+		"S\xC3\xB6""der", "Tall", "Tratt", "Troll", "Upp", "Var",
+		"V\xC3\xA4ster", "\xC3\x84ngel", "\xC3\x96ster",
+	};
+
+	static const char * const names_2a[] = {
+		"B", "Br", "D", "Dr", "Dv", "F", "Fj", "Fl", "Fr", "G", "Gl",
+		"Gn", "Gr", "H", "J", "K", "Kl", "Kn", "Kr", "Kv", "L", "M",
+		"N", "P", "Pl", "Pr", "R", "S", "Sk", "Skr", "Sl", "Sn", "Sp",
+		"Spr", "St", "Str", "Sv", "T", "Tr", "Tv", "V", "Vr",
+	};
+
+	static const char * const names_2b[] = {
+		"a", "e", "i", "o", "u", "y",
+		"\xC3\xA5", "\xC3\xA4", "\xC3\xB6",
+	};
+
+	static const char * const names_2c[] = {
+		"ck", "d",  "dd", "g",  "gg", "l", "ld", "m",  "n",
+		"nd", "ng", "nn", "p",  "pp", "r", "rd", "rk", "rp",
+		"rr", "rt", "s",  "sk", "st", "t", "tt", "v",
+	};
+
+	static const char * const names_3[] = {
+		"arp", "berg", "boda", "borg", "bro", "bukten", "by", "byn",
+		"fors", "hammar", "hamn", "holm", "hus", "h\xC3\xA4ttan",
+		"kulle", "k\xC3\xB6ping", "lund", "l\xC3\xB6v", "sala",
+		"skrona", "sl\xC3\xA4tt", "sp\xC3\xA5ng", "stad", "sund",
+		"svall", "svik", "s\xC3\xA5ker", "udde", "valla", "viken",
+		"\xC3\xA4lv", "\xC3\xA5s",
+	};
+
 	/* optional first segment */
-	append_opt (buf, _name_swedish_1, seed, 0, 50);
+	append_opt (buf, names_1, seed, 0, 50);
 
 	/* mandatory middle segments including option of hardcoded name */
 	if (SeedChance(4, 5, seed) >= 3) {
-		buf->append (choose_str (_name_swedish_2, seed, 7));
+		buf->append (choose_str (names_2, seed, 7));
 	} else {
-		buf->append (choose_str (_name_swedish_2a, seed,  7));
-		buf->append (choose_str (_name_swedish_2b, seed, 10));
-		buf->append (choose_str (_name_swedish_2c, seed, 13));
+		buf->append (choose_str (names_2a, seed,  7));
+		buf->append (choose_str (names_2b, seed, 10));
+		buf->append (choose_str (names_2c, seed, 13));
 	}
 
-	buf->append (choose_str (_name_swedish_3, seed, 16));
+	buf->append (choose_str (names_3, seed, 16));
 }
 
 
