df6bf7290178e60909134ccb51b76467c153a096
cirdan <cirdansw@gmail.com>
2016-04-15 20:19:47 +0200

    Move _name_catalan_*
    Move _name_catalan_* into MakeCatalanTownName.

--- src/townnamegen.cpp
+++ src/townnamegen.cpp
@@ -1861,29 +1861,91 @@ static void MakeItalianTownName (stringb *buf, uint32 seed)
  */
 static void MakeCatalanTownName (stringb *buf, uint32 seed)
 {
+	static const char * const names_real[] = {
+		"Barcelona", "L'Hospitalet de Llobregat", "Cerdanyola",
+		"Martorell", "Badalona", "Tarragona", "Lleida", "Girona",
+		"Sabadell", "Terrassa", "Reus", "Valls", "Vic",
+		"Vielha e Mijaran", "Amposta", "Tortosa", "Berga", "Olot",
+		"Mollerussa", "Banyoles", "Figueres", "Balaguer",
+		"Vilafranca del Pened\xC3\xA8s", "La Seu d'Urgell",
+		"El Pont de Suert", "Igualada", "Manresa", "Solsona",
+		"Les Borges Blanques", "Tremp", "Sort", "Colera", "Portbou",
+		"El Vendrell", "Falset", "Ripoll", "Cervera", "Gandesa",
+		"Matar\xC3\xB3", "Montblanc", "Vilanova i la Geltr\xC3\xBA",
+		"T\xC3\xA0rrega", "Camprodon", "Campdev\xC3\xA0nol",
+		"Cambrils", "Begur", "Setcases", "Palafrugell", "Begues",
+		"El Bruc", "Cadaqu\xC3\xA9s", "Collbat\xC3\xB3",
+		"Cervell\xC3\xB3", "Esparreguera", "Abrera", "Alp", "Das",
+		"Cercs", "Manlleu", "El Masnou", "Molins de Rei", "Monistrol",
+		"Rocallaura", "Rub\xC3\xAD", "Ripollet", "Sitges", "Roses",
+	};
+
+	static const char * const names_pref[] = {
+		"El Pont de ", "Parets de ", "Canet de ", "Castellar de ",
+		"Corbera de ", "Arenys de ", "Calella de ", "La Seu de ",
+		"La Bisbal de ", "Torroella de ", "Port de ", "Vilafranca de ",
+		"Vilanova de ", "Caldes de ", "La Conca de ", "Olesa de ",
+		"La Roca de ", "Sant Esteve de ", "Sant Andreu de ",
+		"Sant Jordi de ", "Sant Joan de ", "Sant Feliu de ",
+		"Sant Quirze de ", "Sant Sadurn\xC3\xAD de ",
+		"Santa Coloma de ", "Santa Margarida de ", "Santa Maria de ",
+		"Sant Mart\xC3\xAD de ", "Sant Pere de ",
+		"Sant Juli\xC3\xA0 de ", "Sant Vicen\xC3\xA7 de ",
+	};
+
+	static const char * const names_1m[] = {
+		"Torrent", "Cami", "Mont", "Bell", "Puig", "Riu",
+	};
+
+	static const char * const names_1f[] = {
+		"Pala",  "Selva", "Vall", "Serra",
+		"Torre", "Riba",  "Cova", "Terra",
+	};
+
+	static const char * const names_2m[] = {
+		"alt", "baix", "fosc", "pelat", "vent\xC3\xB3s",
+		"negre", "roig", "gr\xC3\xADs",
+	};
+
+	static const char * const names_2f[] = {
+		"baixa", "alta", "fosca", "clara",
+		"negra", "roja", "grisa", "freda",
+	};
+
+	static const char * const names_3[] = {
+		" Desp\xC3\xAD", " Desvern", " del Cam\xC3\xAD", " de Mar",
+		" de Dalt", " de Baix", " del Vall\xC3\xA8s",
+		" de Bergued\xC3\xA0", " de Conflent", " de la Plana",
+	};
+
+	static const char * const names_river1[] = {
+		" d'Anoia", " de Ter", " de Llobregat", " d'Ebre",
+		" de Segre", " de Francol\xC3\xAD",
+	};
+
 	if (SeedModChance(0, 3, seed) == 0) { // real city names
-		buf->append (choose_str_mod (_name_catalan_real, seed, 4));
+		buf->append (choose_str_mod (names_real, seed, 4));
 		return;
 	}
 
 	if (SeedModChance(0, 2, seed) == 0) { // prefix
-		buf->append (choose_str_mod (_name_catalan_pref, seed, 11));
+		buf->append (choose_str_mod (names_pref, seed, 11));
 	}
 
 	uint i = SeedChance(0, 2, seed);
 	if (i == 0) { // masculine form
-		buf->append (choose_str_mod (_name_catalan_1m, seed,  4));
-		buf->append (choose_str_mod (_name_catalan_2m, seed, 11));
+		buf->append (choose_str_mod (names_1m, seed,  4));
+		buf->append (choose_str_mod (names_2m, seed, 11));
 	} else { // feminine form
-		buf->append (choose_str_mod (_name_catalan_1f, seed,  4));
-		buf->append (choose_str_mod (_name_catalan_2f, seed, 11));
+		buf->append (choose_str_mod (names_1f, seed,  4));
+		buf->append (choose_str_mod (names_2f, seed, 11));
 	}
 
 	if (SeedModChance(15, 5, seed) == 0) {
 		if (SeedModChance(5, 2, seed) == 0) { // generic suffix
-			buf->append (choose_str_mod (_name_catalan_3, seed, 4));
+			buf->append (choose_str_mod (names_3, seed, 4));
 		} else { // river name suffix
-			buf->append (choose_str_mod (_name_catalan_river1, seed, 4));
+			buf->append (choose_str_mod (names_river1, seed, 4));
 		}
 	}
 }
