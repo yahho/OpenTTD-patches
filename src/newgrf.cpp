8434809a5b57477908e8d5f663e950546c7c8408
cirdan <cirdansw@gmail.com>
2016-06-04 18:12:24 +0200

    Allow NewGRFs to set the selection sprite for a bridge
    Allow NewGRFs to define the sprite to use for a bridge in the bridge
    selection window through Action0 property 14.

--- src/newgrf.cpp
+++ src/newgrf.cpp
@@ -2256,6 +2256,11 @@ static ChangeInfoResult BridgeChangeInfo(uint brid, int numinfo, int prop, ByteR
 				bridge->price = buf->ReadWord();
 				break;
 
+			case 0x14: // purchase sprite
+				bridge->sprite = buf->ReadWord();
+				bridge->pal    = buf->ReadWord();
+				break;
+
 			default:
 				ret = CIR_UNKNOWN;
 				break;
