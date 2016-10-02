d1d1b4dad66a7df3c629e3d71fb945cf160baf97
cirdan <cirdansw@gmail.com>
2016-10-02 17:46:09 +0200

    Add a helper struct CompareParentSpritesSSE41
    Add a helper struct CompareParentSpritesSSE41 to compare two parent
    sprites in ViewportSortParentSpritesSSE41; this will allow for some
    code factorisation in next commit.

--- src/viewport_sprite_sorter_sse4.cpp
+++ src/viewport_sprite_sorter_sse4.cpp
@@ -23,11 +23,58 @@
 	#define LOAD_128 _mm_loadu_si128
 #endif
 
+struct CompareParentSpritesSSE41 {
+	const __m128i mask_ptest;
+
+	CompareParentSpritesSSE41 (void) :
+		mask_ptest (_mm_setr_epi8 (-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  0,  0,  0,  0))
+	{
+	}
+
+	bool operator() (const ParentSpriteToDraw *ps1,
+				const ParentSpriteToDraw *ps2) const;
+};
+
+inline bool CompareParentSpritesSSE41::operator()
+	(const ParentSpriteToDraw *ps1, const ParentSpriteToDraw *ps2) const
+{
+	/*
+	 * Original code:
+	 * if (ps->xmax >= ps2->xmin && ps->xmin <= ps2->xmax && // overlap in X?
+	 *     ps->ymax >= ps2->ymin && ps->ymin <= ps2->ymax && // overlap in Y?
+	 *     ps->zmax >= ps2->zmin && ps->zmin <= ps2->zmax) { // overlap in Z?
+	 *
+	 * Above conditions are equivalent to:
+	 * 1/    !( (ps->xmax >= ps2->xmin) && (ps->ymax >= ps2->ymin) && (ps->zmax >= ps2->zmin)   &&    (ps->xmin <= ps2->xmax) && (ps->ymin <= ps2->ymax) && (ps->zmin <= ps2->zmax) )
+	 * 2/    !( (ps->xmax >= ps2->xmin) && (ps->ymax >= ps2->ymin) && (ps->zmax >= ps2->zmin)   &&    (ps2->xmax >= ps->xmin) && (ps2->ymax >= ps->ymin) && (ps2->zmax >= ps->zmin) )
+	 * 3/  !( ( (ps->xmax >= ps2->xmin) && (ps->ymax >= ps2->ymin) && (ps->zmax >= ps2->zmin) ) &&  ( (ps2->xmax >= ps->xmin) && (ps2->ymax >= ps->ymin) && (ps2->zmax >= ps->zmin) ) )
+	 * 4/ !( !( (ps->xmax <  ps2->xmin) || (ps->ymax <  ps2->ymin) || (ps->zmax <  ps2->zmin) ) && !( (ps2->xmax <  ps->xmin) || (ps2->ymax <  ps->ymin) || (ps2->zmax <  ps->zmin) ) )
+	 * 5/ PTEST <---------------------------------- rslt1 ---------------------------------->         <------------------------------ rslt2 -------------------------------------->
+	 */
+	__m128i ps1_max = LOAD_128((const __m128i*) &ps1->xmax);
+	__m128i ps2_min = LOAD_128((const __m128i*) &ps2->xmin);
+	__m128i rslt1 = _mm_cmplt_epi32 (ps1_max, ps2_min);
+	if (!_mm_testz_si128 (this->mask_ptest, rslt1)) return true;
+
+	__m128i ps1_min = LOAD_128((const __m128i*) &ps1->xmin);
+	__m128i ps2_max = LOAD_128((const __m128i*) &ps2->xmax);
+	__m128i rslt2 = _mm_cmplt_epi32 (ps2_max, ps1_min);
+	if (!_mm_testz_si128 (this->mask_ptest, rslt2)) return false;
+
+	/* Use X+Y+Z as the sorting order, so sprites closer to the bottom of
+	 * the screen and with higher Z elevation, are drawn in front. Here
+	 * X,Y,Z are the coordinates of the "center of mass" of the sprite,
+	 * i.e. X=(left+right)/2, etc. However, since we only care about
+	 * order, don't actually divide / 2. */
+	return  ps1->xmin + ps1->xmax + ps1->ymin + ps1->ymax + ps1->zmin + ps1->zmax <=
+		ps2->xmin + ps2->xmax + ps2->ymin + ps2->ymax + ps2->zmin + ps2->zmax;
+}
+
 /** Sort parent sprites pointer array using SSE4.1 optimizations. */
 void ViewportSortParentSpritesSSE41 (ParentSpriteToDraw **psd,
 	const ParentSpriteToDraw *const *psdvend)
 {
-	const __m128i mask_ptest = _mm_setr_epi8(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  0,  0,  0,  0);
+	CompareParentSpritesSSE41 comparator;
 	while (psd != psdvend) {
 		ParentSpriteToDraw * const ps = *psd;
 
@@ -43,42 +90,7 @@ void ViewportSortParentSpritesSSE41 (ParentSpriteToDraw **psd,
 
 			if (ps2->comparison_done) continue;
 
-			/*
-			 * Decide which comparator to use, based on whether the bounding boxes overlap
-			 *
-			 * Original code:
-			 * if (ps->xmax >= ps2->xmin && ps->xmin <= ps2->xmax && // overlap in X?
-			 *     ps->ymax >= ps2->ymin && ps->ymin <= ps2->ymax && // overlap in Y?
-			 *     ps->zmax >= ps2->zmin && ps->zmin <= ps2->zmax) { // overlap in Z?
-			 *
-			 * Above conditions are equivalent to:
-			 * 1/    !( (ps->xmax >= ps2->xmin) && (ps->ymax >= ps2->ymin) && (ps->zmax >= ps2->zmin)   &&    (ps->xmin <= ps2->xmax) && (ps->ymin <= ps2->ymax) && (ps->zmin <= ps2->zmax) )
-			 * 2/    !( (ps->xmax >= ps2->xmin) && (ps->ymax >= ps2->ymin) && (ps->zmax >= ps2->zmin)   &&    (ps2->xmax >= ps->xmin) && (ps2->ymax >= ps->ymin) && (ps2->zmax >= ps->zmin) )
-			 * 3/  !( ( (ps->xmax >= ps2->xmin) && (ps->ymax >= ps2->ymin) && (ps->zmax >= ps2->zmin) ) &&  ( (ps2->xmax >= ps->xmin) && (ps2->ymax >= ps->ymin) && (ps2->zmax >= ps->zmin) ) )
-			 * 4/ !( !( (ps->xmax <  ps2->xmin) || (ps->ymax <  ps2->ymin) || (ps->zmax <  ps2->zmin) ) && !( (ps2->xmax <  ps->xmin) || (ps2->ymax <  ps->ymin) || (ps2->zmax <  ps->zmin) ) )
-			 * 5/ PTEST <---------------------------------- rslt1 ---------------------------------->         <------------------------------ rslt2 -------------------------------------->
-			 */
-			__m128i ps1_max = LOAD_128((__m128i*) &ps->xmax);
-			__m128i ps2_min = LOAD_128((__m128i*) &ps2->xmin);
-			__m128i rslt1 = _mm_cmplt_epi32(ps1_max, ps2_min);
-			if (!_mm_testz_si128(mask_ptest, rslt1))
-				continue;
-
-			__m128i ps1_min = LOAD_128((__m128i*) &ps->xmin);
-			__m128i ps2_max = LOAD_128((__m128i*) &ps2->xmax);
-			__m128i rslt2 = _mm_cmplt_epi32(ps2_max, ps1_min);
-			if (_mm_testz_si128(mask_ptest, rslt2)) {
-				/* Use X+Y+Z as the sorting order, so sprites closer to the bottom of
-				 * the screen and with higher Z elevation, are drawn in front.
-				 * Here X,Y,Z are the coordinates of the "center of mass" of the sprite,
-				 * i.e. X=(left+right)/2, etc.
-				 * However, since we only care about order, don't actually divide / 2
-				 */
-				if (ps->xmin + ps->xmax + ps->ymin + ps->ymax + ps->zmin + ps->zmax <=
-						ps2->xmin + ps2->xmax + ps2->ymin + ps2->ymax + ps2->zmin + ps2->zmax) {
-					continue;
-				}
-			}
+			if (comparator (ps, ps2)) continue;
 
 			/* Move ps2 in front of ps */
 			ParentSpriteToDraw * const temp = ps2;
