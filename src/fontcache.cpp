b0529fb0b21fa0881d7a75c1ad3dda8c96ac9b29
cirdan <cirdansw@gmail.com>
2016-11-22 18:32:38 +0100

    Merge src/table/unicode.h into src/fontcache.cpp
    Merge the contents of src/table/unicode.h into
    SpriteFontCache::InitializeUnicodeGlyphMap.

--- src/fontcache.cpp
+++ src/fontcache.cpp
@@ -23,7 +23,6 @@
 
 #include "table/sprites.h"
 #include "table/control_codes.h"
-#include "table/unicode.h"
 
 static const int MAX_FONT_SIZE     = 72; ///< Maximum font size.
 
@@ -118,6 +117,35 @@ void SpriteFontCache::SetUnicodeGlyph(GlyphID key, SpriteID sprite)
 
 void SpriteFontCache::InitializeUnicodeGlyphMap()
 {
+	struct DefaultUnicodeMapping {
+		WChar code; ///< Unicode value
+		byte key;   ///< Character index of sprite
+	};
+
+	static const byte CLRA = 0; ///< Identifier to clear all glyphs at this codepoint
+
+	/* Default unicode mapping table for sprite based glyphs.
+	 * This table allows us use unicode characters even though the glyphs don't
+	 * exist, or are in the wrong place, in the standard sprite fonts.
+	 * This is not used for FreeType rendering */
+	static const DefaultUnicodeMapping _default_unicode_map[] = {
+		{ 0x00A0, 0x20 }, // Non-breaking space / Up arrow
+		{ 0x00AA, CLRA }, // Feminine ordinal indicator / Down arrow
+		{ 0x00AC, CLRA }, // Not sign / Tick mark
+		{ 0x00AD, 0x20 }, // Soft hyphen / X mark
+		{ 0x00AF, CLRA }, // Macron / Right arrow
+		{ 0x00B4, CLRA }, // Acute accent / Train symbol
+		{ 0x00B5, CLRA }, // Micro sign / Truck symbol
+		{ 0x00B6, CLRA }, // Pilcrow sign / Bus symbol
+		{ 0x00B7, CLRA }, // Middle dot / Aircraft symbol
+		{ 0x00B8, CLRA }, // Cedilla / Ship symbol
+		{ 0x00B9, CLRA }, // Superscript 1 / Superscript -1
+		{ 0x00BC, CLRA }, // One quarter / Small up arrow
+		{ 0x00BD, CLRA }, // One half / Small down arrow
+		{ 0x0178, 0x9F }, // Capital letter Y with diaeresis
+		{ 0x010D, 0x63 }, // Small letter c with caron
+	};
+
 	static const uint ASCII_LETTERSTART = 32; ///< First printable ASCII letter.
 
 	/* Font sprites are contiguous and arranged by font size. */
