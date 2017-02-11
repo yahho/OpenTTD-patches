8ee1b291dab1a71e1fef759b382ac80b8e428933
cirdan <cirdansw@gmail.com>
2017-02-11 17:27:33 +0100

    Move class CargoMovementAmount to cargopacket.cpp
    Move class CargoMovementAmount to cargopacket.cpp, as it is only
    used there.

--- src/cargoaction.h
+++ src/cargoaction.h
@@ -14,38 +14,4 @@
 
 #include "cargopacket.h"
 
-/** Cargo movement amount tracking class. */
-class CargoMovementAmount {
-private:
-	uint amount;    ///< Amount of cargo still unprocessed.
-
-public:
-	CargoMovementAmount (uint amount) : amount (amount)
-	{
-	}
-
-	/** Get the amount of cargo still unprocessed. */
-	uint Amount (void) const
-	{
-		return this->amount;
-	}
-
-	/**
-	 * Decides if a packet needs to be split.
-	 * @param cp Packet to be either split or moved in one piece.
-	 * @return Either new packet if splitting was necessary or the given
-	 *         one otherwise.
-	 */
-	CargoPacket *Preprocess (CargoPacket *cp)
-	{
-		if (this->amount < cp->Count()) {
-			cp = cp->Split (this->amount);
-			this->amount = 0;
-		} else {
-			this->amount -= cp->Count();
-		}
-		return cp;
-	}
-};
-
 #endif /* CARGOACTION_H */
--- src/cargopacket.cpp
+++ src/cargopacket.cpp
@@ -496,6 +496,40 @@ void VehicleCargoList::InvalidateCache()
 }
 
 
+/** Cargo movement amount tracking class. */
+class CargoMovementAmount {
+private:
+	uint amount;    ///< Amount of cargo still unprocessed.
+
+public:
+	CargoMovementAmount (uint amount) : amount (amount)
+	{
+	}
+
+	/** Get the amount of cargo still unprocessed. */
+	uint Amount (void) const
+	{
+		return this->amount;
+	}
+
+	/**
+	 * Decides if a packet needs to be split.
+	 * @param cp Packet to be either split or moved in one piece.
+	 * @return Either new packet if splitting was necessary or the given
+	 *         one otherwise.
+	 */
+	CargoPacket *Preprocess (CargoPacket *cp)
+	{
+		if (this->amount < cp->Count()) {
+			cp = cp->Split (this->amount);
+			this->amount = 0;
+		} else {
+			this->amount -= cp->Count();
+		}
+		return cp;
+	}
+};
+
 /** Cargo removal amount tracking class. */
 class CargoRemovalAmount {
 private:
