3925acce1ddd7fbef3026956d5be8796d4fde360
cirdan <cirdansw@gmail.com>
2017-04-10 17:47:10 +0200

    Extract blitter switching code from SwitchNewGRFBlitter
    Extract the blitter switching code from SwitchNewGRFBlitter into
    its own function SwitchBlitter.

--- src/gfxinit.cpp
+++ src/gfxinit.cpp
@@ -315,6 +315,26 @@ static const char *SelectNewGRFBlitter (void)
 }
 
 /**
+ * Switch to a new blitter.
+ * @param name The blitter to switch to.
+ * @param old The old blitter in case we have to switch back.
+ * @return False if switching failed and the old blitter could not be restored.
+ */
+static bool SwitchBlitter (const char *name, const char *old)
+{
+	Blitter *new_blitter = Blitter::select (name);
+	/* Blitter::select only fails if it cannot find a blitter by the given
+	 * name, and all of the replacement blitters should be available. */
+	assert (new_blitter != NULL);
+
+	if (VideoDriver::GetActiveDriver()->AfterBlitterChange()) return true;
+
+	/* Failed to switch blitter, let's hope we can return to the old one. */
+	return (Blitter::select (old) != NULL)
+		&& VideoDriver::GetActiveDriver()->AfterBlitterChange();
+}
+
+/**
  * Check blitter needed by NewGRF config and switch if needed.
  * @return False when nothing changed, true otherwise.
  */
@@ -331,18 +351,8 @@ static bool SwitchNewGRFBlitter()
 	if (strcmp (repl_blitter, cur_blitter) == 0) return false;
 
 	DEBUG(misc, 1, "Switching blitter from '%s' to '%s'... ", cur_blitter, repl_blitter);
-	Blitter *new_blitter = Blitter::select (repl_blitter);
-	/* Blitter::select only fails if it cannot find a blitter by
-	 * the given name, and all of the replacement blitters in the
-	 * replacement list should be available. */
-	assert (new_blitter != NULL);
-	DEBUG(misc, 1, "Successfully switched to %s.", repl_blitter);
-
-	if (!VideoDriver::GetActiveDriver()->AfterBlitterChange()) {
-		/* Failed to switch blitter, let's hope we can return to the old one. */
-		if (Blitter::select (cur_blitter) == NULL || !VideoDriver::GetActiveDriver()->AfterBlitterChange()) {
-			usererror("Failed to reinitialize video driver. Specify a fixed blitter in the config");
-		}
+	if (!SwitchBlitter (repl_blitter, cur_blitter)) {
+		usererror ("Failed to reinitialize video driver. Specify a fixed blitter in the config.");
 	}
 
 	return true;
