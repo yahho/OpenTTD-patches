yahho/OpenTTD-patches

commit c53720a189c53f53f26b09ed9bc0df0f15bcdfb5

Pass EngineID vector plus count to DrawEngineList
Pass the vector of engines to draw in DrawEngineList as a pointer
plus count, so that DrawEngineList does not need to know anything
about GUIEngineList.

src/autoreplace_gui.cpp
@@ -29,7 +29,7 @@
 #include "widgets/autoreplace_widget.h"
 
 void DrawEngineList (VehicleType type, BlitArea *dpi, int x, int r, int y,
-	const GUIEngineList *eng_list, uint16 min, uint16 max,
+	const EngineID *eng, uint num,
 	EngineID selected_id, bool show_count, GroupID selected_group);
 
 static int CDECL EngineNumberSorter(const EngineID *a, const EngineID *b)
@@ -414,7 +414,8 @@ public:
 
 				/* Do the actual drawing */
 				DrawEngineList ((VehicleType)this->window_number, dpi, r.left + WD_FRAMERECT_LEFT, r.right - WD_FRAMERECT_RIGHT, r.top + WD_FRAMERECT_TOP,
-						&this->engines[side], start, end, this->sel_engine[side], side == 0, this->sel_group);
+						this->engines[side].Get(start), end - start,
+						this->sel_engine[side], side == 0, this->sel_group);
 				break;
 			}
 		}

src/build_vehicle_gui.cpp
@@ -871,21 +871,17 @@ int DrawVehiclePurchaseInfo (BlitArea *dpi, int left, int right, int y, EngineID
  * @param l The left most location of the list
  * @param r The right most location of the list
  * @param y The top most location of the list
- * @param eng_list What engines to draw
- * @param min where to start in the list
- * @param max where in the list to end
+ * @param eng What engines to draw
+ * @param num Number of engines to draw
  * @param selected_id what engine to highlight as selected, if any
  * @param show_count Whether to show the amount of engines or not
  * @param selected_group the group to list the engines of
  */
 void DrawEngineList (VehicleType type, BlitArea *dpi, int l, int r, int y,
-	const GUIEngineList *eng_list, uint16 min, uint16 max,
+	const EngineID *eng, uint num,
 	EngineID selected_id, bool show_count, GroupID selected_group)
 {
 	static const int sprite_y_offsets[] = { -1, -1, -2, -2 };
-
-	/* Obligatory sanity checks! */
-	assert(max <= eng_list->Length());
 
 	bool rtl = _current_text_dir == TD_RTL;
 	int step_size = GetEngineListHeight(type);
@@ -914,8 +910,8 @@ void DrawEngineList (VehicleType type, BlitArea *dpi, int l, int r, int y,
 	int small_text_y_offset  = step_size - FONT_HEIGHT_SMALL - WD_FRAMERECT_BOTTOM - 1;
 	int replace_icon_y_offset = (step_size - replace_icon.height) / 2 - 1;
 
-	for (; min < max; min++, y += step_size) {
-		const EngineID engine = (*eng_list)[min];
+	for (; num-- > 0; eng++, y += step_size) {
+		const EngineID engine = *eng;
 		/* Note: num_engines is only used in the autoreplace GUI, so it is correct to use _local_company here. */
 		const uint num_engines = GetGroupNumEngines(_local_company, selected_group, engine);
 
@@ -1420,9 +1416,18 @@ struct BuildVehicleWindow : Window {
 	void DrawWidget (BlitArea *dpi, const Rect &r, int widget) const OVERRIDE
 	{
 		switch (widget) {
-			case WID_BV_LIST:
-				DrawEngineList (this->vehicle_type, dpi, r.left + WD_FRAMERECT_LEFT, r.right - WD_FRAMERECT_RIGHT, r.top + WD_FRAMERECT_TOP, &this->eng_list, this->vscroll->GetPosition(), min (this->vscroll->GetPosition() + this->vscroll->GetCapacity(), this->eng_list.Length()), this->sel_engine, false, DEFAULT_GROUP);
+			case WID_BV_LIST: {
+				uint i = this->vscroll->GetPosition();
+				uint j = min (i + this->vscroll->GetCapacity(), this->eng_list.Length());
+				DrawEngineList (this->vehicle_type, dpi,
+						r.left + WD_FRAMERECT_LEFT,
+						r.right - WD_FRAMERECT_RIGHT,
+						r.top + WD_FRAMERECT_TOP,
+						this->eng_list.Get(i), j - i,
+						this->sel_engine, false,
+						DEFAULT_GROUP);
 				break;
+			}
 
 			case WID_BV_SORT_ASCENDING_DESCENDING:
 				this->DrawSortButtonState (dpi, WID_BV_SORT_ASCENDING_DESCENDING, this->descending_sort_order ? SBS_DOWN : SBS_UP);

commit 55e0788ced7d7fdfc9b18763df966a17580b88d1

Fix computation of DrawEngineList arguments
Commit c53720a189c53f53f26b09ed9bc0df0f15bcdfb5 introduced a bug
that could cause an out-of-bounds access if the engine list was
cleared but the scrollbar position was not reset, as then the
position could be greater than the list length and an underflow
could happen when computing the number of engines to draw.

src/autoreplace_gui.cpp
@@ -412,12 +412,14 @@ public:
 			case WID_RV_RIGHT_MATRIX: {
 				int side = (widget == WID_RV_LEFT_MATRIX) ? 0 : 1;
 				EngineID start  = this->vscroll[side]->GetPosition(); // what is the offset for the start (scrolling)
-				EngineID end    = min(this->vscroll[side]->GetCapacity() + start, this->engines[side].Length());
-
+				EngineID end    = this->engines[side].Length();
 				/* Do the actual drawing */
-				DrawEngineList ((VehicleType)this->window_number, dpi, r.left + WD_FRAMERECT_LEFT, r.right - WD_FRAMERECT_RIGHT, r.top + WD_FRAMERECT_TOP,
-						this->engines[side].Get(start), end - start,
+				if (start < end) {
+					uint n = min (end - start, this->vscroll[side]->GetCapacity());
+					DrawEngineList ((VehicleType)this->window_number, dpi, r.left + WD_FRAMERECT_LEFT, r.right - WD_FRAMERECT_RIGHT, r.top + WD_FRAMERECT_TOP,
+						this->engines[side].Get(start), n,
 						this->sel_engine[side], side == 0, this->sel_group);
+				}
 				break;
 			}
 		}

src/build_vehicle_gui.cpp
@@ -1416,14 +1416,17 @@ struct BuildVehicleWindow : Window {
 		switch (widget) {
 			case WID_BV_LIST: {
 				uint i = this->vscroll->GetPosition();
-				uint j = min (i + this->vscroll->GetCapacity(), this->eng_list.Length());
-				DrawEngineList (this->vehicle_type, dpi,
+				uint j = this->eng_list.Length();
+				if (i < j) {
+					uint n = min (j - i, this->vscroll->GetCapacity());
+					DrawEngineList (this->vehicle_type, dpi,
 						r.left + WD_FRAMERECT_LEFT,
 						r.right - WD_FRAMERECT_RIGHT,
 						r.top + WD_FRAMERECT_TOP,
-						this->eng_list.Get(i), j - i,
+						this->eng_list.Get(i), n,
 						this->sel_engine, false,
 						DEFAULT_GROUP);
+					}
 				break;
 			}
 